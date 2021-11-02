f5dc825e3c0f95df453b423cd34238969afd2aff
ingong <insong1216@gmail.com>
2021-11-02 14:09:29 +0900

    chore: Webpack 설정 추가

---
+++ client/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+
+  entry: './src/index',
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jp(e)g|gif|svg)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'assets',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024,
+          },
+        },
+      },
+      {
+        resolve: {
+          alias: {
+            '@': path.resolve(__dirname, 'src'),
+          },
+        },
+      },
+    ],
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify:
+        process.env.NODE_ENV === 'production'
+          ? {
+              collapseWhitespace: true,
+              removeComments: true, 
+            }
+          : false,
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
