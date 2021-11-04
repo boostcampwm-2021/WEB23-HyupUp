09515826c1dd7b834ae777ecc6735c8349307794
ingong <insong1216@gmail.com>
2021-11-04 19:05:20 +0900

    feat: App, index.tsx React 초기 렌더링 [#1]

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from './styles/GlobalStyle';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div>Hello World</div>
+    </>
+  );
+}
+
+export default App;
