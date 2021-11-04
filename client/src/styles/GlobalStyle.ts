6f899802515c40b9e4d13e02eb52ca217611bee7
ingong <insong1216@gmail.com>
2021-11-04 19:05:52 +0900

    layout: GlobalStyle, theme 추가, 기본 글꼴 Pretendard 설정 [#1]

---
+++ client/src/styles/GlobalStyle.ts
@@ -0,0 +1,75 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import PretendardRegular from '@/styles/fonts/Pretendard-Regular.woff2';
+import PretendardBold from '@/styles/fonts/Pretendard-Bold.woff2';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+  }
+
+  html{
+    font-size: 10px;
+  }
+  @media screen and (max-width: 1440px) {
+    html {
+      font-size: 8px;
+    }
+  }
+  @media screen and (max-width: 1024px) {
+    html {
+      font-size: 6px;
+    }
+  }
+
+  @font-face {
+    font-family: 'Pretendard';	
+    src: local('PretendardRegular'),
+    url(${PretendardRegular}) format('woff2');
+    font-weight: 300; 		
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Pretendard';	
+    src: local('PretendardBold'),
+    url(${PretendardBold}) format('woff2');
+    font-weight: 600; 		
+    font-style: normal;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    
+    font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
+    padding: 0;
+    max-height: 100vh;
+    overflow: overlay;
+    -ms-overflow-style: none;
+    max-width: 1140px;
+    margin: 0 auto;
+    
+    ::-webkit-scrollbar {
+      width: 6px;
+    }
+    ::-webkit-scrollbar-thumb {
+      border-radius: 6px;
+      background-color: rgba(223, 223, 223, 0.6);
+    
+      &:hover {
+        background-color: rgba(193, 193, 193, 0.6);
+      }
+    }
+
+  };
+
+  a { color: #fff; text-decoration: none; outline: none }
+  a:hover, a:active { text-decoration: none; color:#fff; background-color: transparent; }
+  button{ outline: none; background: none; border: none; cursor: pointer; };
+  input { border: none; outline: none; }
+`;
+
+export default GlobalStyle;
---
+++ client/src/styles/theme.ts
@@ -0,0 +1,42 @@
+const color = {
+  white: '#FFFFFF',
+  gray100: '#FAFAFA',
+  gray200: '#DDE2E8',
+  gray300: '#8993A1',
+  gray400: '#30333E',
+  gray500: '#1C2229',
+  blue100: '#E2EEFF',
+  blue200: '#95BBF2',
+  blue300: '#568EE2',
+  blue400: '#2D6ECF',
+  blue500: '#255FB5',
+  red100: '#FFE7E7',
+  red200: '#EFA8A5',
+  red300: '#EE8B87',
+  red400: '#F06E69',
+  red500: '#F73B3B',
+  green100: '#E0FFE8',
+  green200: '#9DE9B1',
+  green300: '#5FCC7C',
+  green400: '#4FAAB67',
+  green500: '#408E54',
+};
+
+const font = {
+  body_regular: "normal 300 16px/auto 'Pretendard'",
+  body_medium: "normal 300 18px/auto 'Pretendard'",
+  body_large: "normal 300 22px/auto 'Pretendard'",
+  bold_regular: "normal 600 16px/auto 'Pretendard'",
+  bold_medium: "normal 600 18px/auto 'Pretendard'",
+  bold_large: "normal 600 22px/auto 'Pretendard'",
+  display_small: "normal 600 24px/auto 'Pretendard'",
+  display_medium: "normal 600 30px/auto 'Pretendard'",
+  display_large: "normal 600 48px/auto 'Pretendard'",
+};
+
+const theme = {
+  color,
+  font,
+};
+
+export default theme;
