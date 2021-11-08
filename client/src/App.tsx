f94079bca9881e1ded9124279e6320600eedd085
ingong <insong1216@gmail.com>
2021-11-08 19:26:41 +0900

    feat: props 값에 따른 크기 및 색상 변경 버튼 컴포넌트 생성 [#24]

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,15 +6,8 @@ function App() {
     <>
       <GlobalStyle />
       <div>
-        <Button
-          border="none"
-          color="pink"
-          height="200px"
-          onClick={() => console.log('You clicked on the pink circle!')}
-          radius="50%"
-          width="200px"
-        >
-          {"I'm a pink circle!"}
+        <Button variant="default" size="small">
+          버튼
         </Button>
         <p>Hello World</p>
       </div>
--- client/src/lib/design/Button.tsx
+++ client/src/lib/design/Button.tsx
@@ -1,29 +1,76 @@
 import React from 'react';
+import styled, { css, FlattenSimpleInterpolation } from 'styled-components';
 
-type Props = {
-  border: string;
-  color: string;
-  height: string;
-  width: string;
-  radius: string;
-  onClick: () => void;
-  children?: React.ReactNode;
+type Styling = {
+  [key: string]: FlattenSimpleInterpolation;
 };
 
-const Button = ({ border, color, height, width, radius, onClick, children }: Props) => {
+const SIZES: Styling = {
+  small: css`
+    --button-font-size: 12px;
+    --button-padding: 10px 15px;
+  `,
+  large: css`
+    --button-font-size: 16px;
+    --button-padding: 13px 50px;
+  `,
+};
+
+const VARIANTS: Styling = {
+  default: css`
+    --button-color: #fafafa;
+    --button-bg-color: #255fb5;
+    --button-hover-bg-color: #fafafa;
+    &:hover {
+      --button-color: #255fb5;
+      --button-bg-color: #fafafa;
+    }
+  `,
+  confirm: css`
+    --button-color: #fafafa;
+    --button-bg-color: #f06e69;
+  `,
+  cancel: css`
+    --button-color: #8993a1;
+    --button-bg-color: #fafafa;
+  `,
+};
+
+interface LayoutProps {
+  variantStyle: FlattenSimpleInterpolation;
+  sizeStyle: FlattenSimpleInterpolation;
+}
+
+const StyledButton = styled.button<LayoutProps>`
+  ${(p) => p.sizeStyle};
+  ${(p) => p.variantStyle};
+  margin: 0;
+  border: none;
+  cursor: pointer;
+
+  padding: var(--button-padding);
+  border-radius: var(--button-radius, 8px);
+  color: var(--button-color, #ffffff);
+  background: var(--button-bg-color, #0d6efd);
+  font: ${({ theme }) => theme.font.bold_regular};
+  font-size: var(--button-font-size, 1rem);
+`;
+
+type Props = {
+  variant: string;
+  size: string;
+  children: string;
+  disabled?: boolean;
+};
+
+const Button = ({ variant, size, children, disabled }: Props) => {
+  const variantStyle = VARIANTS[variant];
+  const sizeStyle = SIZES[size];
+
   return (
-    <button
-      onClick={onClick}
-      style={{
-        backgroundColor: color,
-        border,
-        borderRadius: radius,
-        height,
-        width,
-      }}
-    >
+    <StyledButton variantStyle={variantStyle} sizeStyle={sizeStyle} disabled={disabled}>
       {children}
-    </button>
+    </StyledButton>
   );
 };
 
