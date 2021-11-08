5a8dbd75e331460faa4dcb2cdf9a60cde80b1a14
ingong <insong1216@gmail.com>
2021-11-08 23:07:49 +0900

    fix: App 컴포넌트 버튼 제거 [#24]

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import GlobalStyle from './styles/GlobalStyle';
-import Button from '@/lib/design/Button';
+
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>
-        <Button variant="default" size="small">
-          버튼
-        </Button>
-        <p>Hello World</p>
-      </div>
+      <div>Hello World</div>
     </>
   );
 }
