6b55d72e8e02fa2bc465c9098d19bd108ebe2da0
rudy3091 <grayblack313@gmail.com>
2021-11-09 20:48:09 +0900

    refactor: Roadmap 컴포넌트 스타일 분리
    style 파일과 컴포넌트 로직 분리

--- client/src/components/Roadmap/index.tsx
+++ client/src/components/Roadmap/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import styled from 'styled-components';
 import Button from '@/lib/design/Button';
+import S from './style';
 
 const Roadmap = () => {
   return (
@@ -19,58 +19,3 @@ const Roadmap = () => {
 };
 
 export default Roadmap;
-
-const S = {
-  Container: styled.div`
-    display: flex;
-    flex-direction: column;
-
-    width: 941px;
-    margin: 75px 0 0 26px;
-    padding: 18px;
-
-    border-radius: 8px;
-    background-color: ${({ theme }) => theme.color.gray100};
-
-    font: ${({ theme }) => theme.font.body_regular};
-  `,
-  Title: styled.h2`
-    margin-bottom: 16px;
-
-    font: ${({ theme }) => theme.font.bold_medium};
-  `,
-  Content: styled.section`
-    display: flex;
-
-    width: 100%;
-    height: 100%;
-  `,
-  ContentEntry: styled.aside`
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    width: 206px;
-    height: 100%;
-    margin-right: 16px;
-
-    background-color: ${({ theme }) => theme.color.white};
-
-    & > button {
-      position: absolute;
-      bottom: 0;
-
-      width: calc(100% - 16px);
-      margin: 8px;
-    }
-  `,
-  MainContent: styled.section`
-    position: relative;
-
-    width: 100%;
-    height: 100%;
-
-    background-color: ${({ theme }) => theme.color.white};
-  `,
-};
