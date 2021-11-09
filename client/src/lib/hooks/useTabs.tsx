efe88186d05296810095281c7d89500e6b8236e5
rudy3091 <grayblack313@gmail.com>
2021-11-09 18:14:38 +0900

    feat: useTabs 커스텀 훅 수정 [#16]
    현재 보고있는 화면의 인덱스까지 반환하도록 해 SideBar 컴포넌트 내부의
    어떤 항목이 활성화되어야 하는지 알 수 있도록 구현

--- client/src/lib/hooks/useTabs.tsx
+++ client/src/lib/hooks/useTabs.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 function useTabs(initialTabIndex: number, tabs: React.ReactNode[]) {
   const [currentIndex, setCurrentIndex] = React.useState<number>(initialTabIndex);
   return {
+    currentIndex,
     currentTab: tabs[currentIndex],
     changeTab: setCurrentIndex,
   };
