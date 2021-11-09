acc97a718c4df72579a5d771a8184ec0c04627cd
Harry <haesoo9410@gmail.com>
2021-11-09 17:28:01 +0900

    feat: 더미데이터로 유저 정보반환 api 제작 [#42] 유저 정보 요청시 test 메일주소에 맞는 유저정보를 클라이언트에 반환하도록 api 설계

--- server/src/Users/Users.controller.ts
+++ server/src/Users/Users.controller.ts
@@ -1,5 +1,39 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
-export const handleGet = (req: Request, res: Response) => {
-  res.send('ok');
+interface User {
+  id: number;
+  name: string;
+  job: string;
+  email: string;
+  url: string;
+  admin: boolean;
+  organization: number;
+}
+
+const dummyUrl1 =
+  'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80';
+const dummyUrl2 =
+  'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80';
+
+export const handleGet = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const email = req.body.email;
+
+    // db로직
+    const isTest1: boolean = email === 'test1@gmail.com';
+    const user: User = {
+      id: isTest1 ? 1 : 2,
+      name: isTest1 ? 'harry' : 'jarry',
+      job: isTest1 ? 'FE' : 'BE',
+      email: email,
+      url: isTest1 ? dummyUrl1 : dummyUrl2,
+      admin: isTest1 ? true : false,
+      organization: 1,
+    };
+
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ message: '유저 정보 없음' });
+    next(err);
+  }
 };
