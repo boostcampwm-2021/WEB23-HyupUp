179cc0816448b5ef488d90ff77ce43398659deee
이찬호 <chanholee275@gmail.com>
2021-11-09 11:31:06 +0900

    refactor: class를 추가하는 형식에서 조건부 렌더링으로 변경 [#24]
    class 이름을 추가하여 css를 적용하는 방식을 props를 활용한 조건부 렌더링으로 변경

--- client/src/lib/design/DropDown.tsx
+++ client/src/lib/design/DropDown.tsx
@@ -1,26 +1,32 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import arrow from '@public/arrow_drop_down.png';
 import theme from '@/styles/theme';
+import { useState } from 'react';
 
-const Box = styled.ul`
+interface Props {
+  state: boolean;
+}
+
+const Box = styled.ul<Props>`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
 
-  visibility: hidden;
-  opacity: 0;
+  visibility: ${(props) => (props.state ? 'visible' : 'hidden')};
+  opacity: ${(props) => (props.state ? '1' : '0')};
   top: 100%;
   left: 0;
 
   border-radius: 8px;
 
-  transform: translateY(-10px);
-  z-index: -1;
+  transform: ${(props) => (props.state ? 'translateY(0px)' : 'translateY(-10px)')};
+  z-index: ${(props) => (props.state ? '1' : '-1')};
   transition: all 0.3s ease-in-out 0s, visibility 0s linear 0.3s, z-index 0s linear 0.01s;
   box-shadow: 0px 4px 16px 4px rgba(0, 0, 0, 0.2);
+  transition-delay: ${(props) => (props.state ? '0s' : ' 0s, 0s, 0.3s;')};
 `;
 
 const Item = styled.li`
@@ -30,9 +36,9 @@ const Item = styled.li`
   font: ${(props) => props.theme.font};
 `;
 
-const ArrowImage = styled.img`
-  transform: rotate(90deg);
-  transition-duration: 0.1s;
+const ArrowImage = styled.img<Props>`
+  transform: ${(props) => (props.state ? 'rotate(0deg)' : 'rotate(90deg)')};
+  transition-duration: ${(props) => (props.state ? '0.1s' : '0.1s')};
 `;
 
 const Parent = styled.div`
@@ -41,18 +47,6 @@ const Parent = styled.div`
   height: fit-content;
 
   border-radius: 8px;
-
-  .active-ul {
-    visibility: visible;
-    opacity: 1;
-    z-index: 1;
-    transform: translateY(0px);
-    transition-delay: 0s, 0s, 0.3s;
-  }
-  .active-image {
-    transform: rotate(0deg);
-    transition-duration: 0.1s;
-  }
 `;
 
 const ContextContainer = styled.div`
@@ -80,14 +74,6 @@ interface dropDownProps {
   handleClick: (e: React.MouseEvent) => void;
 }
 
-const changeClassName = (component: HTMLElement | null, className: string) => {
-  if (component?.classList.contains(className)) {
-    component.classList.remove(className);
-  } else {
-    component?.classList.add(className);
-  }
-};
-
 /**
  * DropDown Component를 반환하는 함수
  * @param props title, list, click, font를 key로 가지고 있는 객체
@@ -99,30 +85,26 @@ const DropDown = (props: dropDownProps) => {
   const fontTheme = {
     font: font ? font : theme.font.body_regular,
   };
-
-  const listRef = useRef<HTMLUListElement>(null);
-  const imageRef = useRef<HTMLImageElement>(null);
-  const targetRef = useRef<HTMLDivElement>(null);
+  const [clickState, clickStateHandler] = useState(false);
 
   const changeState = (e: React.MouseEvent) => {
     e.preventDefault();
-    changeClassName(listRef.current, 'active-ul');
-    changeClassName(imageRef.current, 'active-image');
+    clickStateHandler(!clickState);
   };
 
   const selectItem = (e: React.MouseEvent) => {
     e.preventDefault();
-    changeState(e);
+    clickStateHandler(!clickState);
     handleClick(e);
   };
 
   return (
     <Parent>
-      <ContextContainer onClick={changeState} ref={targetRef}>
+      <ContextContainer onClick={changeState}>
         {Title}
-        <ArrowImage src={arrow} ref={imageRef} />
+        <ArrowImage src={arrow} state={clickState} />
       </ContextContainer>
-      <Box onClick={selectItem} ref={listRef}>
+      <Box onClick={selectItem} state={clickState}>
         {list.map((el: string, i: number) =>
           i === list.length - 1 ? (
             <Item key={i} theme={fontTheme}>
