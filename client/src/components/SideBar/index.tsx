e1c7db6b309590b3e03f995fb407d965448dd562
rudy3091 <grayblack313@gmail.com>
2021-11-09 20:50:29 +0900

    refactor: SideBar 스타일 분리
    SideBar 컴포넌트의 스타일과 로직 분리

--- client/src/components/SideBar/index.tsx
+++ client/src/components/SideBar/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import styled from 'styled-components';
+import S from './style';
 
 import Dropdown from '@/lib/design/DropDown';
 
@@ -30,29 +30,3 @@ const SideBar = ({ entries, changeTab }: SideBarProps) => {
 };
 
 export default SideBar;
-
-const S = {
-  Container: styled.section`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    width: 173px;
-    height: 600px;
-    padding: 24px 16px;
-    margin-top: 75px;
-
-    background-color: ${({ theme }) => theme.color.gray100};
-    border-radius: 8px;
-
-    font: ${({ theme }) => theme.font.body_regular};
-  `,
-  Entry: styled.ul`
-    margin-top: 32px;
-  `,
-  EntryItem: styled.li`
-    padding: 12px 0;
-
-    color: ${({ theme }) => theme.color.gray300};
-  `,
-};
