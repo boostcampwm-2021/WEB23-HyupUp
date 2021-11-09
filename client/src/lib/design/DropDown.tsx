67d4a877f7e1e782434225d5b136e8a60ed51450
rudy3091 <grayblack313@gmail.com>
2021-11-09 17:50:03 +0900

    fix: 드롭다운 컴포넌트 스타일링 문제 해결
    드롭다운 항목이 자리를 차지하던 문제 해결
    스타일링이 깨지는 문제까지 해결함

--- client/src/lib/design/DropDown.tsx
+++ client/src/lib/design/DropDown.tsx
@@ -22,6 +22,7 @@ const Box = styled.ul<Props>`
   top: 100%;
   left: 0;
 
+  background-color: ${({ theme }) => theme.color.white};
   border-radius: 8px;
 
   transform: ${(props) => (props.state ? 'translateY(0px)' : 'translateY(-10px)')};
@@ -32,7 +33,7 @@ const Box = styled.ul<Props>`
 `;
 
 const Item = styled.li`
-  width: fit-content;
+  min-width: 100px;
   margin: 16px;
 
   font: ${(props) => props.theme.font};
