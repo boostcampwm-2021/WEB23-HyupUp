47139f3a2effb42b95b5d695ee75aaa43c098e10
rudy3091 <grayblack313@gmail.com>
2021-11-09 18:10:59 +0900

    feat: useTabs를 이용해 workpage 내부에서 화면을 바꿀 수 있도록 구현 [#16]
    useTabs 훅의 반환값으로 받아온 함수를 SideBar의 props로 넘겨줘 내부의
    항목을 클릭하면 화면이 바뀌도록 구현

--- client/src/pages/WorkPage.tsx
+++ client/src/pages/WorkPage.tsx
@@ -4,12 +4,18 @@ import styled from 'styled-components';
 import SideBar from '@/components/SideBar';
 import SideBarEntry from '@/components/SideBarEntry';
 import Roadmap from '@/components/Roadmap';
+import Kanban from '@/components/Kanban';
+import Backlog from '@/components/Backlog';
+import useTabs from '@/lib/hooks/useTabs';
 
 import roadmap from '@public/icons/calendar-icon.svg';
 import board from '@public/icons/board-icon.svg';
 import backlog from '@public/icons/time-icon.svg';
 
 const WorkPage = () => {
+  const tabs = [<Roadmap></Roadmap>, <Kanban></Kanban>, <Backlog></Backlog>];
+  const { currentTab, changeTab } = useTabs(0, tabs);
+
   const sideBarEntries = [
     <SideBarEntry icon={roadmap} name={'로드맵'} />,
     <SideBarEntry icon={board} name={'칸반보드'} />,
@@ -17,8 +23,8 @@ const WorkPage = () => {
   ];
   return (
     <S.Container>
-      <SideBar entries={sideBarEntries}></SideBar>
-      <Roadmap></Roadmap>
+      <SideBar entries={sideBarEntries} changeTab={changeTab} highlight={0}></SideBar>
+      {currentTab}
     </S.Container>
   );
 };
