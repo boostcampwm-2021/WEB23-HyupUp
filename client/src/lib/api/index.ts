e64f5c08e686dc96a6d40c4d36c6cd40ad67e653
Harry <haesoo9410@gmail.com>
2021-11-09 20:30:34 +0900

    feat: 더미 데이터를 받아오는 axios api 요청 추가 [#42]

---
+++ client/src/lib/api/index.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { UserState } from '@/contexts/User';
+
+const instance = axios.create({
+  baseURL: 'http://localhost:3000',
+  withCredentials: true,
+});
+
+export const getUser = async (email: string) => {
+  try {
+    const result: { data: UserState } = await instance.get(`/api/users?email=${email}`);
+    // 추후 로그인된이메일 확인은 세션에서 (get요청이므로 데이터 없애야함)
+    return result.data;
+  } catch (e) {
+    console.error('유저 정보 요청 실패');
+    throw e;
+  }
+};
