de940388d55db7f121ccc18060a13c71aa9fcbc1
rudy3091 <grayblack313@gmail.com>
2021-11-09 18:10:59 +0900

    feat: useTabs를 이용해 workpage 내부에서 화면을 바꿀 수 있도록 구현 [#16]
    useTabs 훅의 반환값으로 받아온 함수를 SideBar의 props로 넘겨줘 내부의
    항목을 클릭하면 화면이 바뀌도록 구현

--- client/src/components/SideBar/index.tsx
+++ client/src/components/SideBar/index.tsx
@@ -5,22 +5,30 @@ import Dropdown from '@/lib/design/DropDown';
 
 interface SideBarProps {
   entries: React.ReactNode[];
+  changeTab: (tabIndex: number) => void;
   highlight?: number;
 }
 
 /**
  *
  * @param entries 사이드바에 항목으로 들어갈 리액트 컴포넌트들의 배열
+ * @param changeTab 현재 보고있는 화면을 로드맵, 칸반보드, 백로그 페이지에서 바꿔줄 함수
  * @param highlight? 활성화될 항목의 인덱스 번호
  * @returns
  */
-const SideBar = ({ entries, highlight }: SideBarProps) => {
+const SideBar = ({ entries, changeTab, highlight }: SideBarProps) => {
   return (
     <S.Container>
       <Dropdown Title={'프로젝트 선택'} list={['1', '2', '3']} handleClick={(e) => {}}></Dropdown>
       <S.Entry>
         {entries.map((entry, i) => (
-          <S.EntryItem highlight={i === highlight ? true : false}>{entry}</S.EntryItem>
+          <S.EntryItem
+            key={entry?.toString()}
+            highlight={i === highlight ? true : false}
+            onClick={() => changeTab(i)}
+          >
+            {entry}
+          </S.EntryItem>
         ))}
       </S.Entry>
     </S.Container>
@@ -51,6 +59,6 @@ const S = {
   EntryItem: styled.li<{ highlight: boolean }>`
     padding: 12px 0;
 
-    color: ${({ theme }) => theme.color.gray400};
+    color: ${({ highlight, theme }) => (highlight ? theme.color.blue400 : theme.color.gray300)};
   `,
 };
