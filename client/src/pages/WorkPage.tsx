94037d06911121e65e047ec815d9444b47fcd814
rudy3091 <grayblack313@gmail.com>
2021-11-09 16:43:04 +0900

    layout: WorkPage 컴포넌트의 아이콘 수정 [#16]
    WorkPage 컴포넌트에서 잘못 사용되고 있던 아이콘 수정
    home-icon -> calendar-icon

--- client/src/pages/WorkPage.tsx
+++ client/src/pages/WorkPage.tsx
@@ -5,13 +5,13 @@ import SideBar from '@/components/SideBar';
 import SideBarEntry from '@/components/SideBarEntry';
 import Roadmap from '@/components/Roadmap';
 
-import home from '@public/icons/home-icon.svg';
+import roadmap from '@public/icons/calendar-icon.svg';
 import board from '@public/icons/board-icon.svg';
 import backlog from '@public/icons/time-icon.svg';
 
 const WorkPage = () => {
   const sideBarEntries = [
-    <SideBarEntry icon={home} name={'로드맵'} />,
+    <SideBarEntry icon={roadmap} name={'로드맵'} />,
     <SideBarEntry icon={board} name={'칸반보드'} />,
     <SideBarEntry icon={backlog} name={'백로그'} />,
   ];
