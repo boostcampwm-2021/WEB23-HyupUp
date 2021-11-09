45114f0f4f0e80d8b2c3ccf0dcb5d79c9fe4ef04
rudy3091 <grayblack313@gmail.com>
2021-11-09 16:17:45 +0900

    feat: WorkPage 페이지 컴포넌트 수정 [#16]
    WorkPage 페이지에서 사이드바를 표시하도록 뼈대를 추가함

--- client/src/pages/WorkPage.tsx
+++ client/src/pages/WorkPage.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import SideBar from '@/components/SideBar';
+import SideBarEntry from '@/components/SideBarEntry';
+
+import home from '@public/icons/home-icon.svg';
+import board from '@public/icons/board-icon.svg';
+import backlog from '@public/icons/time-icon.svg';
 
 const WorkPage = () => {
-  return <div>WorkPage</div>;
+  const sideBarEntries = [
+    <SideBarEntry icon={home} name={'로드맵'} />,
+    <SideBarEntry icon={board} name={'칸반보드'} />,
+    <SideBarEntry icon={backlog} name={'백로그'} />,
+  ];
+  return (
+    <>
+      <div>this will be work page</div>
+      <SideBar entries={sideBarEntries}></SideBar>
+    </>
+  );
 };
 
 export default WorkPage;
