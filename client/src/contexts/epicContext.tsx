2993fe21eb66b50ab4e5b784ff2520acb48e060c
이찬호 <chanholee275@gmail.com>
2021-11-09 21:15:58 +0900

    feat: epic 관련 정보를 context api를 활용해 전역 변수로 선언 [#14]
    특정 project의 epic을 관리하기 위하여 전역 변수 선언

---
+++ client/src/contexts/epicContext.tsx
@@ -0,0 +1,87 @@
+import React, { createContext, Dispatch, useContext, useReducer } from 'react';
+
+type Project = {
+  id: number;
+  name: string;
+};
+type Epic = {
+  id: number;
+  name: string;
+  startAt: Date;
+  endAt: Date;
+  projects: Project;
+};
+
+type State = {
+  [index: string]: Array<Epic>;
+};
+type Action =
+  | { type: 'ADD_EPIC'; epic: Epic }
+  | { type: 'REMOVE_EPIC'; epic: Epic }
+  | { type: 'UPDATE_EPIC'; epic: Epic };
+
+type EpicDispatch = Dispatch<Action>;
+
+const EpicStateContext = createContext<State | null>(null);
+const EpicDispatchContext = createContext<EpicDispatch | null>(null);
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'ADD_EPIC':
+      if (Object.keys(state).includes(action.epic.projects.name)) {
+        state[action.epic.projects.name].push(action.epic);
+        return { ...state };
+      } else {
+        state[action.epic.projects.name] = [action.epic];
+        return { ...state };
+      }
+    case 'REMOVE_EPIC':
+      if (Object.keys(state).includes(action.epic.projects.name)) {
+        state[action.epic.projects.name] = state[action.epic.projects.name].filter(
+          (el) => el.id !== action.epic.id,
+        );
+        return { ...state };
+      } else {
+        throw new Error('invalid project name');
+      }
+    case 'UPDATE_EPIC':
+      if (Object.keys(state).includes(action.epic.projects.name)) {
+        state[action.epic.projects.name] = state[action.epic.projects.name].map((el) => {
+          if (el.id === action.epic.id) {
+            el.name = action.epic.name;
+            el.startAt = action.epic.startAt;
+            el.endAt = action.epic.endAt;
+            return el;
+          } else {
+            return el;
+          }
+        });
+        return { ...state };
+      } else {
+        throw new Error('invalid project name');
+      }
+    default:
+      throw new Error('unhandled action');
+  }
+}
+
+export function EpicProvider({ children }: { children: React.ReactNode }) {
+  const [state, dispatch] = useReducer(reducer, {});
+  return (
+    <EpicStateContext.Provider value={state}>
+      <EpicDispatchContext.Provider value={dispatch}>{children}</EpicDispatchContext.Provider>
+    </EpicStateContext.Provider>
+  );
+}
+
+export function useEpicState() {
+  const state = useContext(EpicStateContext);
+  if (!state) throw new Error('Cannot find EpicProvider'); // 유효하지 않을땐 에러를 발생
+  return state;
+}
+
+export function useEpicDispatch() {
+  const dispatch = useContext(EpicDispatchContext);
+  if (!dispatch) throw new Error('Cannot find EpicProvider'); // 유효하지 않을땐 에러를 발생
+  return dispatch;
+}
