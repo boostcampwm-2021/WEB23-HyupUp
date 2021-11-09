a47735a6d93ded873f2cacea0e7f6ea49b9e3ac4
이찬호 <chanholee275@gmail.com>
2021-11-09 17:26:58 +0900

    fix: task entity에 project id column 추가 [#52]

--- server/src/Tasks/Tasks.entity.ts
+++ server/src/Tasks/Tasks.entity.ts
@@ -1,6 +1,7 @@
 import Stories from '../Stories/Stories.entity';
 import Users from '../Users/Users.entity';
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import Projects from '../Projects/Projects.entity';
 
 @Entity({ name: 'TASKS' })
 export default class Tasks {
@@ -12,6 +13,10 @@ export default class Tasks {
 
   @Column({ name: 'STATUS', type: 'boolean' })
   status!: boolean;
+
+  @ManyToOne(() => Projects, (projects) => projects.id)
+  @JoinColumn({ name: 'PROJECT_ID' })
+  projects!: Projects;
 
   @ManyToOne(() => Stories, (stories) => stories.id)
   @JoinColumn({ name: 'STORY_ID' })
