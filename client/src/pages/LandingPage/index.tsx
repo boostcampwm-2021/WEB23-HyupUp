8391b6f2eea978f304cb1ef90b456e267ad0dfad
Harry <haesoo9410@gmail.com>
2021-11-09 15:54:36 +0900

    layout: 테스트용 로그인 버튼 레이아웃  추가 [#42] 테스트를 위한 로그인 버튼 레이아웃 추가

--- client/src/pages/LandingPage/index.tsx
+++ client/src/pages/LandingPage/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Body, Container, Logo, TextContainer, Title } from './style';
+import Button from '@/lib/design/Button';
 
 const LandingPage = () => {
   return (
@@ -10,6 +11,24 @@ const LandingPage = () => {
         <Title>일 잘하는 사람들을 위한</Title>
         <Title>가장 간편한 협업 툴</Title>
       </TextContainer>
+      <Button
+        size="large"
+        category="default"
+        onClick={() => {
+          console.log('test1');
+        }}
+      >
+        Login as Test1
+      </Button>
+      <Button
+        size="large"
+        category="default"
+        onClick={() => {
+          console.log('test1');
+        }}
+      >
+        Login as Test2
+      </Button>
       <TextContainer>
         <Body>로그인은 개인 정보 보호 정책 및 서비스 약관에 동의하는 것을 의미하며,</Body>
         <Body>서비스 이용을 위해 이메일과 이름, 프로필 이미지를 수집합니다.</Body>
