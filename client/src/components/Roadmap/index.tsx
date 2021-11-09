5fc8d99d8e48b3fb89154d2578404488bd53b7ad
rudy3091 <grayblack313@gmail.com>
2021-11-09 16:38:42 +0900

    feat: Roadmap 컴포넌트 추가 [#12]
    로드맵 페이지의 UI를 담당하는 Roadmap 컴포넌트 추가

---
+++ client/src/components/Roadmap/index.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import Button from '@/lib/design/Button';
+
+const Roadmap = () => {
+  return (
+    <S.Container>
+      <S.Title>프로젝트 로드맵</S.Title>
+      <S.Content>
+        <S.ContentEntry>
+          <Button size={'small'} category={'cancel'} onClick={() => {}}>
+            Add item
+          </Button>
+        </S.ContentEntry>
+        <S.MainContent>content</S.MainContent>
+      </S.Content>
+    </S.Container>
+  );
+};
+
+export default Roadmap;
+
+const S = {
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 100%;
+    margin: 75px 0 0 26px;
+    padding: 18px;
+
+    border-radius: 8px;
+    background-color: ${({ theme }) => theme.color.gray100};
+
+    font: ${({ theme }) => theme.font.body_regular};
+  `,
+  Title: styled.h2`
+    margin-bottom: 16px;
+
+    font: ${({ theme }) => theme.font.bold_medium};
+  `,
+  Content: styled.section`
+    display: flex;
+
+    width: 100%;
+    height: 100%;
+  `,
+  ContentEntry: styled.aside`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 206px;
+    height: 100%;
+    margin-right: 16px;
+
+    background-color: ${({ theme }) => theme.color.white};
+
+    & > button {
+      position: absolute;
+      bottom: 0;
+
+      width: calc(100% - 16px);
+      margin: 8px;
+    }
+  `,
+  MainContent: styled.section`
+    position: relative;
+
+    width: 100%;
+    height: 100%;
+
+    background-color: ${({ theme }) => theme.color.white};
+  `,
+};
