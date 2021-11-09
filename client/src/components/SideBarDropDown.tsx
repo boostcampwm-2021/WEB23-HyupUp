313b61c00a91077bc3ea5915e2d6e2572c5aa9bf
이찬호 <chanholee275@gmail.com>
2021-11-09 16:57:25 +0900

    feat: 작업 페이지의 프로젝트 선택을 위한 드롭 다운 컴포넌트 작성 [#10]

---
+++ client/src/components/SideBarDropDown.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import DropDown from '@/lib/design/DropDown';
+import { useState } from 'react';
+import { useEffect } from 'react';
+const Title = styled.p`
+  margin: 5px;
+
+  font: ${({ theme }) => theme.font.body_regular};
+`;
+
+interface Project {
+  id: number;
+  name: string;
+}
+const SideBarDropDown = () => {
+  const SERVER_URL = 'http://localhost:3000';
+  const [listState, listStateHandler] = useState([]);
+  const [titleState, titleStateHandler] = useState(<Title>프로젝트</Title>);
+  const itemClickHandler = (e: React.MouseEvent) => {
+    const event = e.target as HTMLElement;
+    if (event.tagName === 'LI') {
+      titleStateHandler(<Title>{event.innerText}</Title>);
+    }
+  };
+  useEffect(() => {
+    fetch(`${SERVER_URL}/api/projects?userId=${1}&organizationId=${1}`)
+      .then((res) => res.json())
+      .then((projectList) => projectList.map((el: Project) => el.name))
+      .then((nameList) => {
+        listStateHandler(nameList);
+      })
+      .catch(() => {
+        window.location.reload();
+      });
+  }, []);
+  return (
+    <div>
+      <DropDown Title={titleState} list={listState} handleClick={itemClickHandler} />
+    </div>
+  );
+};
+
+export default SideBarDropDown;
