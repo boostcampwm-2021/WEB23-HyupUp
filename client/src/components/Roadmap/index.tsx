8321c4d0f754c450f7e8751d66b7cba98bf9cf16
rudy3091 <grayblack313@gmail.com>
2021-11-11 00:05:25 +0900

    feat: 로드맵 화면에서 에픽을 추가할 수 있도록 구현 [#15]
    - Add item 버튼을 클릭하면 input 컴포넌트가 보여짐
    - input 컴포넌트에서 입력을 완료하고 엔터를 누르면 서버로 요청이 전송됨
    - 서버에서 생성에 성공했으면 생성된 에픽의 id값을 반환함
    - 이때 epicContext 에도 생성된 epic을 추가해줘야 하는데, userContext와
      연동이 안된 상태라 불가능함
    - 에픽을 생성한 뒤 새로고침하면 생성된 에픽이 정상적으로 나타남
    - 프로젝트 이름이 하드코딩된 상태라 수정이 필요함

--- client/src/components/Roadmap/index.tsx
+++ client/src/components/Roadmap/index.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import Button from '@/lib/design/Button';
 import S from './style';
-import { useEpicState } from '@/lib/hooks/useContextHooks';
+import EpicPlaceholder from '../EpicPlaceholder';
+import { useEpicDispatch, useEpicState } from '@/lib/hooks/useContextHooks';
+import { createEpic } from '@/lib/api/epic';
 
 const Roadmap = () => {
+  const [inputVisible, setInputVisible] = React.useState(false);
   const epics = useEpicState();
   // TODO: 현재 프로젝트 이름을 context에서 받아오도록 수정
   const epicsOnProject = epics['HyupUp'];
+  const epicDispatcher = useEpicDispatch();
 
   return (
     <S.Container>
@@ -16,7 +20,34 @@ const Roadmap = () => {
           {epicsOnProject?.map(({ name }, i) => (
             <S.EpicEntryItem key={name + i.toString()}>{name}</S.EpicEntryItem>
           ))}
-          <Button size={'small'} category={'cancel'} onClick={() => undefined}>
+          <EpicPlaceholder
+            visible={inputVisible}
+            handleSubmit={async (value) => {
+              // TODO: 프로젝트 이름을 전달해서 작동하도록 수정
+              const { id } = await createEpic(1, value);
+              // FIXME: 에픽 입력을 완료했을 때 에픽 생성에 성공했다면 context 에 추가해주는 로직
+              // userContext와 연동을 하지 못한 상태라 Project를 넣어줄 수 없음
+              //
+              // if (id !== -1) {
+              //   epicDispatcher({
+              //     type: 'ADD_EPIC',
+              //     epic: {
+              //       id: id,
+              //       name: value,
+              //       startAt: new Date(),
+              //       endAt: new Date(),
+              //       projects: null, // <--
+              //     },
+              //   });
+              // }
+              setInputVisible(false);
+            }}
+          ></EpicPlaceholder>
+          <Button
+            size={'small'}
+            category={'cancel'}
+            onClick={() => setInputVisible((prevState) => !prevState)}
+          >
             + Add item
           </Button>
         </S.EpicEntry>
