45f448a23aec316d2cc6fed9431759d669890cd9
Harry <haesoo9410@gmail.com>
2021-11-10 14:56:47 +0900

    refactor: 스타일 코드 객체화작업진행 묶어서 관리하기 위해 객체화함

--- client/src/pages/LandingPage/style.ts
+++ client/src/pages/LandingPage/style.ts
@@ -1,35 +1,37 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
+export const Styled = {
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
 
-  height: 100vh;
-`;
+    height: 100vh;
+  `,
 
-export const Logo = styled.p`
-  font: ${({ theme }) => theme.font.display_medium};
-`;
+  Logo: styled.p`
+    font: ${({ theme }) => theme.font.display_medium};
+  `,
 
-export const Title = styled.p`
-  margin: 5px;
+  Title: styled.p`
+    margin: 5px;
 
-  font: ${({ theme }) => theme.font.display_large};
-`;
+    font: ${({ theme }) => theme.font.display_large};
+  `,
 
-export const Body = styled.p`
-  margin: 5px;
+  Body: styled.p`
+    margin: 5px;
 
-  color: ${({ theme }) => theme.color300};
+    color: ${({ theme }) => theme.color300};
 
-  font: ${({ theme }) => theme.font.body_small};
-`;
+    font: ${({ theme }) => theme.font.body_small};
+  `,
 
-export const TextContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
-`;
+  TextContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+  `,
+};
