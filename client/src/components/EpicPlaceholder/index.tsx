fb1a19c2eb3c4e5191f5b5b3d231a5c0b5261f6c
rudy3091 <grayblack313@gmail.com>
2021-11-10 23:43:57 +0900

    feat: EpicPlaceholder 컴포넌트에 value 상태 추가 [#15]
    - 일반적인 input 컴포넌트처럼 작동하도록 구현
    - props로 넘겨주는 handleSubmit 함수를 통해 api 요청을 보낼 수 있도록 함

--- client/src/components/EpicPlaceholder/index.tsx
+++ client/src/components/EpicPlaceholder/index.tsx
@@ -3,10 +3,29 @@ import S from './style';
 
 interface EpicPlaceholderProps {
   visible: boolean;
+  handleSubmit(epicName: string): void;
 }
 
-const EpicPlaceholder = ({ visible }: EpicPlaceholderProps) => {
-  return <S.Container>{visible ? <input type="text" /> : undefined}</S.Container>;
+const EpicPlaceholder = ({ visible, handleSubmit }: EpicPlaceholderProps) => {
+  const [value, setValue] = React.useState<string>('');
+
+  const handleEnter = (ev: React.KeyboardEvent) => {
+    if (value === '' || ev.key !== 'Enter') return;
+    handleSubmit(value);
+    setValue('');
+  };
+
+  const handleChange = (ev: React.ChangeEvent) => {
+    setValue((ev.target as HTMLInputElement).value);
+  };
+
+  return (
+    <S.Container>
+      {visible ? (
+        <input type="text" value={value} onKeyPress={handleEnter} onChange={handleChange} />
+      ) : undefined}
+    </S.Container>
+  );
 };
 
 export default EpicPlaceholder;
