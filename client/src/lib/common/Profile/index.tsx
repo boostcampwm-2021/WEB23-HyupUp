13071ad3c7f1ba79a4f43e2060f7c34d24c99407
ingong <insong1216@gmail.com>
2021-11-10 15:09:57 +0900

    refactor: 프로필 컴포넌트 콜백함수 제거 및 함수명 수정 [#41]

--- client/src/lib/common/Profile/index.tsx
+++ client/src/lib/common/Profile/index.tsx
@@ -4,23 +4,20 @@ import useOutSideClick from '@/lib/hooks/useOutSideClick';
 
 const Profile = () => {
   const [openState, setOpenState] = useState(false);
-  const toggling = () => setOpenState((openState) => !openState);
-  const logOutHandler = () => console.log('Logout');
+  const toggleDropDown = () => setOpenState((openState) => !openState);
+  const handleOutClick = () => setOpenState((openState) => !openState);
+  const handleLogout = () => console.log('Logout');
   const ref = useRef(null);
-
-  const handleOutClick = () => {
-    toggling();
-  };
 
   useOutSideClick(ref, handleOutClick);
   return (
     <Styled.Profile>
-      <section onClick={toggling}></section>
+      <section onClick={toggleDropDown}></section>
       {openState && (
         <div className="list-container" ref={ref}>
           <ul className="dropdown-list">
             <li>이름</li>
-            <li className="logout" onClick={logOutHandler}>
+            <li className="logout" onClick={handleLogout}>
               로그아웃
             </li>
           </ul>
