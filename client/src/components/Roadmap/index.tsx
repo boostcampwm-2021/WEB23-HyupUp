e36ad62e832fa861b21ba5a1d9811732aba5c194
rudy3091 <grayblack313@gmail.com>
2021-11-10 23:25:04 +0900

    feat: Roadmap 컴포넌트에서 에픽 상태를 사용하도록 수정 [#15]
    - 예시용으로 남겨놓은 문자열 배열을 모두 삭제
    - context 에서 에픽 리스트를 가져오고, 이를 사용해 화면에 렌더링하도록
    수정

--- client/src/components/Roadmap/index.tsx
+++ client/src/components/Roadmap/index.tsx
@@ -1,33 +1,20 @@
 import * as React from 'react';
 import Button from '@/lib/design/Button';
 import S from './style';
+import { useEpicState } from '@/lib/hooks/useContextHooks';
 
 const Roadmap = () => {
-  const list = [
-    'abcdasdfasdfjasdjfoasjdfojasodfjasodjfojasdfj',
-    'abcdasdfasdfjasdjfoasjdfojasodfjasodjfojasdfj',
-    'abcdasdfasdfjasdjfoasjdfojasodfjasodjfojasdfj',
-    'abcdasdf',
-    'abcdasdf',
-    'abcdasdf',
-    'abcdasdf',
-    'abcdasdf',
-    'abcdasdf',
-    'abcdasdf',
-    'abcdasdf',
-    'abcdasdf',
-    'abcdasdf',
-    'abcdasdf',
-    'abcdasdf',
-  ];
+  const epics = useEpicState();
+  // TODO: 현재 프로젝트 이름을 context에서 받아오도록 수정
+  const epicsOnProject = epics['HyupUp'];
 
   return (
     <S.Container>
       <S.Title>프로젝트 로드맵</S.Title>
       <S.Content>
         <S.EpicEntry>
-          {list.map((s, i) => (
-            <S.EpicEntryItem key={s + i.toString()}>{s}</S.EpicEntryItem>
+          {epicsOnProject?.map(({ name }, i) => (
+            <S.EpicEntryItem key={name + i.toString()}>{name}</S.EpicEntryItem>
           ))}
           <Button size={'small'} category={'cancel'} onClick={() => undefined}>
             + Add item
