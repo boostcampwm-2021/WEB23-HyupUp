86956fe46e593cc0005b0a154e87b4cf76fb4923
이찬호 <chanholee275@gmail.com>
2021-11-10 16:37:42 +0900

    chore: dotenv 관련 파일 설치 및 클라이언트 환경 변수 설정

--- client/src/lib/api/user.ts
+++ client/src/lib/api/user.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 import { UserState } from '@/contexts/userContext';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.SERVER_URL + '/api/users',
   withCredentials: true,
 });
 
 export const getUser = async (email: string) => {
   try {
-    const result: { data: UserState } = await instance.get(`/api/users?email=${email}`);
+    const result: { data: UserState } = await instance.get(`?email=${email}`);
     // 추후 로그인된이메일 확인은 세션에서 (get요청이므로 데이터 없애야함)
     return result.data;
   } catch (e) {
