61ccf6c92a85034f7f9941eee461d61723e28224
ingong <insong1216@gmail.com>
2021-11-10 20:44:09 +0900

    layout: 칸반보드 Column 스타일 추가 [#61]
    Todo, Inprogress, Done 컬럼을 표시한다

--- client/src/components/Kanban/index.tsx
+++ client/src/components/Kanban/index.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import Styled from '@/components/Kanban/style';
 
 const Kanban = () => {
-  return <Styled.Container>this will be kanban board component</Styled.Container>;
+  return (
+    <Styled.Container>
+      <Styled.Title>프로젝트 칸반보드</Styled.Title>
+      <Styled.ColumnContainer>
+        <Styled.Column></Styled.Column>
+        <Styled.Column></Styled.Column>
+        <Styled.Column></Styled.Column>
+      </Styled.ColumnContainer>
+    </Styled.Container>
+  );
 };
 
 export default Kanban;
--- client/src/components/Kanban/style.ts
+++ client/src/components/Kanban/style.ts
@@ -3,13 +3,40 @@ import styled from 'styled-components';
 
 const Styled = {
   Container: styled.section`
+    display: flex;
+    flex-direction: column;
+
     width: 941px;
+
+    margin: 0 0 0 26px;
+    padding: 18px;
+
+    border-radius: 8px;
     background-color: ${({ theme }) => theme.color.gray100};
   `,
 
-  Title: styled.h3``,
+  Title: styled.h3`
+    height: 50px;
+    font: ${({ theme }) => theme.font.bold_medium};
+  `,
 
-  TaskColumn: styled.article``,
+  ColumnContainer: styled.section`
+    width: 100%;
+    height: calc(100% - 50px);
+
+    display: flex;
+    justify-content: space-around;
+
+    font: ${({ theme }) => theme.font.body_regular};
+  `,
+
+  Column: styled.article`
+    width: 30%;
+    height: 100%;
+
+    border-radius: 8px;
+    background-color: ${({ theme }) => theme.color.white};
+  `,
 };
 
 export default Styled;
