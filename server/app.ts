88fdab30fac2a0f317da298df0effabd8b934656
이찬호 <chanholee275@gmail.com>
2021-11-10 22:01:09 +0900

    feat: 할 일을 추가하기 위한 라우터 적용 [#17]

--- server/app.ts
+++ server/app.ts
@@ -14,6 +14,7 @@ import projectRouter from './src/Projects/Projects.router';
 import epicRouter from './src/Epics/Epics.router';
 import storyRouter from './src/Stories/Stories.router';
 import taskRouter from './src/Tasks/Tasks.router';
+import todoRouter from './src/Todo/Todo.router';
 
 const app = express();
 dotenv.config();
@@ -36,6 +37,7 @@ app.use('/api/projects', projectRouter);
 app.use('/api/epics', epicRouter);
 app.use('/api/stories', storyRouter);
 app.use('/api/tasks', taskRouter);
+app.use('/api/todo', todoRouter);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   next(createError(404));
