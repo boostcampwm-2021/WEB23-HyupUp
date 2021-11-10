501772d78c79998872163efb5ea70885b2381b23
Harry <haesoo9410@gmail.com>
2021-11-10 14:56:47 +0900

    refactor: 스타일 코드 객체화작업진행 묶어서 관리하기 위해 객체화함

--- client/src/pages/LandingPage/index.tsx
+++ client/src/pages/LandingPage/index.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import Button from '@/lib/design/Button';
 import { UserContext } from '@/contexts/User';
 import { getUser } from '@/lib/api/user';
-import { Body, Container, Logo, TextContainer, Title } from './style';
+import { Styled } from './style';
 
 const LandingPage = () => {
   const { dispatch } = useContext(UserContext);
@@ -16,13 +16,13 @@ const LandingPage = () => {
   };
 
   return (
-    <Container>
-      <Logo>HyupUp</Logo>
+    <Styled.Container>
+      <Styled.Logo>HyupUp</Styled.Logo>
       <img src="https://img.freepik.com/free-vector/business-team-brainstorm-idea-lightbulb-from-jigsaw-working-team-collaboration-enterprise-cooperation-colleagues-mutual-assistance-concept-pinkish-coral-bluevector-isolated-illustration_335657-1651.jpg?size=626&ext=jpg" />
-      <TextContainer>
-        <Title>일 잘하는 사람들을 위한</Title>
-        <Title>가장 간편한 협업 툴</Title>
-      </TextContainer>
+      <Styled.TextContainer>
+        <Styled.Title>일 잘하는 사람들을 위한</Styled.Title>
+        <Styled.Title>가장 간편한 협업 툴</Styled.Title>
+      </Styled.TextContainer>
       <Button
         size="large"
         category="default"
@@ -41,14 +41,16 @@ const LandingPage = () => {
       >
         Login as Test2
       </Button>
-      <TextContainer>
-        <Body>로그인은 개인 정보 보호 정책 및 서비스 약관에 동의하는 것을 의미하며,</Body>
-        <Body>서비스 이용을 위해 이메일과 이름, 프로필 이미지를 수집합니다.</Body>
-      </TextContainer>
+      <Styled.TextContainer>
+        <Styled.Body>
+          로그인은 개인 정보 보호 정책 및 서비스 약관에 동의하는 것을 의미하며,
+        </Styled.Body>
+        <Styled.Body>서비스 이용을 위해 이메일과 이름, 프로필 이미지를 수집합니다.</Styled.Body>
+      </Styled.TextContainer>
       <a href="http://www.freepik.com">
-        <Body>Designed by vectorjuice / Freepik</Body>
+        <Styled.Body>Designed by vectorjuice / Freepik</Styled.Body>
       </a>
-    </Container>
+    </Styled.Container>
   );
 };
 
