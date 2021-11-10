a798ebddf6229d8ae0f71de0eb3a461cb959af7f
ingong <insong1216@gmail.com>
2021-11-10 23:15:49 +0900

    feat: contextHook 을 활용한 Todo칸반 아이템 렌더링 추가 [#61]

--- client/src/components/Kanban/index.tsx
+++ client/src/components/Kanban/index.tsx
@@ -1,18 +1,49 @@
 import React from 'react';
 import Styled from '@/components/Kanban/style';
 import Button from '@/lib/design/Button';
+import { useStoryState, useStoryDispatch } from '@/lib/hooks/useContextHooks';
 
 const Kanban = () => {
+  const storyState = useStoryState();
+  const useDispatcher = useStoryDispatch();
+  const storyArray = storyState[Object.keys(storyState)[0]];
+  const storyLastId = storyArray ? storyArray[storyArray.length - 1].id : 0;
+
+  const addStoryHandler = () => {
+    useDispatcher({
+      type: 'ADD_STORY',
+      story: {
+        id: storyLastId + 1,
+        name: '',
+        status: 'todo',
+        // todo 필요한지 다시 고민해볼 필요가 있음
+        projects: { id: 1, name: 'test' },
+        epics: {
+          id: 1,
+          name: 'test',
+          startAt: new Date(),
+          endAt: new Date(),
+          projects: { id: 1, name: 'test' },
+        },
+      },
+    });
+  };
+
+  console.log(storyArray);
   return (
     <Styled.Container>
       <Styled.Title>프로젝트 칸반보드</Styled.Title>
       <Styled.ColumnContainer>
         <Styled.Column>
           <h4>To do</h4>
-          <Styled.KanBanItem>
-            <input type="text" placeholder="type a todo..." />
-          </Styled.KanBanItem>
-          <Button size={'large'} category={'cancel'} onClick={() => undefined}>
+          {storyArray?.map((story) => {
+            return (
+              <Styled.KanBanItem key={story.id}>
+                <input type="text" placeholder="type a todo..." />
+              </Styled.KanBanItem>
+            );
+          })}
+          <Button size={'large'} category={'cancel'} onClick={addStoryHandler}>
             Add Todo
           </Button>
         </Styled.Column>
