f73d2a3cd720df27973d8bad1330e97764b60a3a
rudy3091 <grayblack313@gmail.com>
2021-11-11 18:37:56 +0900

    feat: 데이터를 소켓서버로 전송하기 위한 훅 작성 [#15]
    사용법은 jsdoc 주석에 정리함

---
+++ client/src/lib/hooks/useSocketSend.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { SocketContext } from '@/contexts/socketContext';
+
+/**
+ *
+ * @param channel 데이터를 전송할 소켓 채널이름
+ * @returns 파라미터로 전달한 데이터를 channel로 전송해주는 함수
+ * @example
+ * const helloEmit = useSocketSend('hello');
+ * ...
+ * helloEmit('hello world!'); // hello 채널로 'hello world' 문자열을 전송
+ */
+function useSocketSend(channel: string) {
+  const { connection } = React.useContext(SocketContext);
+
+  // emit(1, 2, 3) 과 emit([1, 2, 3]) 이 동일한 기능을 하기 위한 로직
+  return (...dataToEmit: any[]) => {
+    connection.emit(channel, dataToEmit.length ? dataToEmit[0] : dataToEmit);
+  };
+}
+
+export default useSocketSend;
