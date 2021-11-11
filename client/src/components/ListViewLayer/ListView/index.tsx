a5198e2bf969eb015de0a383c4fa89e0e4a9d54c
Harry <haesoo9410@gmail.com>
2021-11-11 19:14:30 +0900

    feat: 리스트아이템이 화면에 출력되도록 구현 [#28]

--- client/src/components/ListViewLayer/ListView/index.tsx
+++ client/src/components/ListViewLayer/ListView/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Styled from '@/components/ListViewLayer/ListView/style';
+import { useUserDispatch, useUserState } from '@/lib/hooks/useContextHooks';
 import ListViewHeader from '@/components/ListViewLayer/ListViewHeader';
+import ListViewItem from '@/components/ListViewLayer/ListViewItem';
 
 const ListView = () => {
+  const state = useUserState();
+  const dispatch = useUserDispatch();
+  const privateTasks = state.privateTasks;
+  const projectTasks = state.projectTasks;
+  const allTasks = [...privateTasks!, ...projectTasks!];
+  console.log(allTasks);
   return (
     <Styled.Container>
       <ListViewHeader />
+      {allTasks.map((item, i) => (
+        <ListViewItem item={item.name} key={i} />
+      ))}
     </Styled.Container>
   );
 };
---
+++ client/src/components/ListViewLayer/ListViewItem/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ListViewItem = ({ item }) => {
+  return <div>{item}</div>;
+};
+
+export default ListViewItem;
