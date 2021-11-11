f857abe9f52b22ec3e5081497336fea4ef820acf
ingong <insong1216@gmail.com>
2021-11-11 22:35:20 +0900

    feat: CreateStory 함수 호출 및 StatusType Enum 으로 변환 [#69]
    - KanbanTodo 에서 CreateStory 함수 호출
    - StatusType 으로 Enum Type 선언

--- client/src/components/KanbanTodo/index.tsx
+++ client/src/components/KanbanTodo/index.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import { useStoryState, useStoryDispatch } from '@/lib/hooks/useContextHooks';
+import { useUserState } from '@/lib/hooks/useContextHooks';
+import { createStory } from '@/lib/api/story';
 import useInput from '@/lib/hooks/useInput';
 import Styled from '@/components/KanbanTodo/style';
 import Button from '@/lib/design/Button';
 
+enum StatusType {
+  TODO,
+  IN_PRGORESS,
+  DONE,
+}
+interface StoryObject {
+  id: number;
+  name: string;
+  status: StatusType;
+}
+
 const KanbanTodo = () => {
   const storyArray = useStoryState();
   const useDispatch = useStoryDispatch();
-  const [value, onChange] = useInput();
+  const userState = useUserState();
 
+  const [value, onChange] = useInput();
   const lastStoryId = storyArray.length > 0 ? storyArray[storyArray.length - 1]?.id : 0;
-  const newStoryObj = {
+  const StoryObject: StoryObject = {
     id: lastStoryId + 1,
     name: '',
-    status: 'todo',
+    status: 'TODO',
   };
+  console.log(userState);
 
   const addStory = () => {
-    useDispatch({
-      type: 'ADD_STORY',
-      story: newStoryObj,
-    });
+    useDispatch({ type: 'ADD_STORY', story: StoryObject });
+    createStory(StoryObject.id, StoryObject.status, userState.currentProjectId as number, '', '');
   };
 
   return (
