301fd589bbc150e8d3fbb6395919843d46586bd5
이찬호 <chanholee275@gmail.com>
2021-11-11 14:18:24 +0900

    refactor: 전역 변수 구조 변경에 따른 코드 수정 [#14]
    전역 변수 구조를 객체이서 배열로 변경, 배열의 타입 변경

--- client/src/contexts/epicContext.tsx
+++ client/src/contexts/epicContext.tsx
@@ -1,74 +1,64 @@
 import React, { createContext, Dispatch, useReducer } from 'react';
+import producer from 'immer';
 
 type Epic = {
   id: number;
   name: string;
   startAt: Date;
   endAt: Date;
-  projectName: string;
 };
 
-type State = {
-  [index: string]: Array<Epic>;
+type UpdateEpic = {
+  id: number;
+  name?: string;
+  startAt?: Date;
+  endAt?: Date;
 };
+
+type State = Array<Epic>;
+
 type Action =
   | { type: 'ADD_EPIC'; epic: Epic }
-  | { type: 'REMOVE_EPIC'; epic: Epic }
-  | { type: 'UPDATE_EPIC'; epic: Epic };
+  | { type: 'REMOVE_EPIC'; id: number }
+  | { type: 'UPDATE_EPIC'; epic: UpdateEpic }
+  | { type: 'DROP_EPIC' };
 
 type EpicDispatch = Dispatch<Action>;
 
 const EpicStateContext = createContext<State | null>(null);
 const EpicDispatchContext = createContext<EpicDispatch | null>(null);
 
-function isProjectExist(state: State, projectName: string): boolean {
-  return Object.keys(state).includes(projectName);
-}
-
 // to-do 필요한 action이 있으면, 아래에 추가할 것
 // to-do immutable 방식을 더 생각해볼 것
 function reducer(state: State, action: Action): State {
   switch (action.type) {
     case 'ADD_EPIC':
-      if (isProjectExist(state, action.epic.projectName)) {
-        state[action.epic.projectName] = [action.epic, ...state[action.epic.projectName]];
-        return { ...state };
-      } else {
-        state[action.epic.projectName] = [action.epic];
-        return { ...state };
-      }
+      return producer(state, (draft) => {
+        draft.push(action.epic);
+      });
     case 'REMOVE_EPIC':
-      if (isProjectExist(state, action.epic.projectName)) {
-        state[action.epic.projectName] = state[action.epic.projectName].filter(
-          (el) => el.id !== action.epic.id,
-        );
-        return { ...state };
-      } else {
-        throw new Error('invalid project name');
-      }
+      return producer(state, (draft) => {
+        return draft.filter((el) => el.id !== action.id);
+      });
     case 'UPDATE_EPIC':
-      if (isProjectExist(state, action.epic.projectName)) {
-        state[action.epic.projectName] = state[action.epic.projectName].map((el) => {
-          if (el.id === action.epic.id) {
-            el.name = action.epic.name;
-            el.startAt = action.epic.startAt;
-            el.endAt = action.epic.endAt;
-            return el;
-          } else {
-            return el;
-          }
+      return producer(state, (draft) => {
+        return draft.map((el) => {
+          if (el.id !== action.epic.id) return el;
+          return {
+            ...el,
+            ...action.epic,
+          };
         });
-        return { ...state };
-      } else {
-        throw new Error('invalid project name');
-      }
+      });
+    case 'DROP_EPIC':
+      return [];
     default:
       throw new Error('unhandled action');
   }
 }
 
 export function EpicProvider({ children }: { children: React.ReactNode }) {
-  const [state, dispatch] = useReducer(reducer, {});
+  const [state, dispatch] = useReducer(reducer, []);
   return (
     <EpicStateContext.Provider value={state}>
       <EpicDispatchContext.Provider value={dispatch}>{children}</EpicDispatchContext.Provider>
--- client/src/contexts/storyContext.tsx
+++ client/src/contexts/storyContext.tsx
@@ -1,73 +1,71 @@
 import React, { createContext, Dispatch, useReducer } from 'react';
+import producer from 'immer';
 
 type Story = {
   id: number;
   name: string;
   status: string;
-  projectName: string;
-  epics: string;
+  epicName?: string;
+  epicId?: number;
 };
 
-type State = {
-  [index: string]: Array<Story>;
+type CreateStory = {
+  id: number;
+  name: string;
 };
+
+type UpdateStory = {
+  id: number;
+  name?: string;
+  status?: string;
+  epicName?: string;
+  epicId?: number;
+};
+
+type State = Array<Story>;
+
 type Action =
-  | { type: 'ADD_STORY'; story: Story }
-  | { type: 'REMOVE_STORY'; story: Story }
-  | { type: 'UPDATE_STORY'; story: Story };
+  | { type: 'ADD_STORY'; story: CreateStory }
+  | { type: 'REMOVE_STORY'; id: number }
+  | { type: 'UPDATE_STORY'; story: UpdateStory }
+  | { type: 'DROP_STORY' };
 
 type StoryDispatch = Dispatch<Action>;
 
 const StoryStateContext = createContext<State | null>(null);
 const StoryDispatchContext = createContext<StoryDispatch | null>(null);
 
-function isProjectExist(state: State, projectName: string): boolean {
-  return Object.keys(state).includes(projectName);
-}
-
 // to-do 필요한 action이 있으면, 아래에 추가할 것
 // to-do immutable 방식을 더 생각해볼 것
 function reducer(state: State, action: Action): State {
   switch (action.type) {
     case 'ADD_STORY':
-      if (isProjectExist(state, action.story.projectName)) {
-        state[action.story.projectName] = [action.story, ...state[action.story.projectName]];
-        return { ...state };
-      } else {
-        state[action.story.projectName] = [action.story];
-        return { ...state };
-      }
+      return producer(state, (draft) => {
+        draft.push({ ...action.story, status: 'TODO' });
+      });
     case 'REMOVE_STORY':
-      if (isProjectExist(state, action.story.projectName)) {
-        state[action.story.projectName] = state[action.story.projectName].filter(
-          (el) => el.id !== action.story.id,
-        );
-        return { ...state };
-      } else {
-        throw new Error('invalid project name');
-      }
+      return producer(state, (draft) => {
+        return draft.filter((el) => el.epicId !== action.id);
+      });
     case 'UPDATE_STORY':
-      if (isProjectExist(state, action.story.projectName)) {
-        state[action.story.projectName] = state[action.story.projectName].map((el) => {
-          if (el.id === action.story.id) {
-            el.name = action.story.name;
-            el.status = action.story.status;
-            return el;
-          } else {
-            return el;
-          }
+      return producer(state, (draft) => {
+        return draft.map((el) => {
+          if (el.id !== action.story.id) return el;
+          return {
+            ...el,
+            ...action.story,
+          };
         });
-        return { ...state };
-      } else {
-        throw new Error('invalid project name');
-      }
+      });
+    case 'DROP_STORY':
+      return [];
     default:
       throw new Error('unhandled action');
   }
 }
 
 export function StoryProvider({ children }: { children: React.ReactNode }) {
-  const [state, dispatch] = useReducer(reducer, {});
+  const [state, dispatch] = useReducer(reducer, []);
   return (
     <StoryStateContext.Provider value={state}>
       <StoryDispatchContext.Provider value={dispatch}>{children}</StoryDispatchContext.Provider>
