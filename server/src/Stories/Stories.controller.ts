bee100df7caf98d7977106ca3cfe80ac796d091d
ingong <insong1216@gmail.com>
2021-11-11 19:09:42 +0900

    feat: Story 생성하는 백엔드 함수 작성 [#69]
    - StoryController 에 Story 추가하는 함수 작성
    - Story 를 생성 시 ProjectID 를 받도록 추가 구현해야함

--- server/src/Stories/Stories.controller.ts
+++ server/src/Stories/Stories.controller.ts
@@ -28,3 +28,30 @@ export const getAllStoriesByProject = async (req: Request, res: Response) => {
     }
   }
 };
+
+/**
+ *
+ * @body projectName: string | number 대상 프로젝트의 id값
+ * @body name: string 새롭게 생성하는 에픽의 이름
+ * @response id: number 새롭게 생성된 에픽의 id값
+ */
+export const createStory = async (req: Request, res: Response) => {
+  try {
+    const result = await getRepository(Stories)
+      .createQueryBuilder()
+      .insert()
+      .into(Stories)
+      .values({
+        id: req.body.storyId,
+        name: req.body.storyName,
+        status: req.body.status,
+        epics: req.body.epicId,
+      })
+      .execute();
+    res.status(201).json({ id: result.raw.insertId });
+  } catch (e) {
+    res.status(400).json({
+      message: (e as Error).message,
+    });
+  }
+};
