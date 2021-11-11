493ee265df0b2cfbfe0ff517d313e0e98de155b0
이찬호 <chanholee275@gmail.com>
2021-11-11 18:30:36 +0900

    feat: 메인 페이지에 동료 상태창 추가 [#36]

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '@/lib/common/Header';
 import TodoInputBar from '@/components/TodoInputBar';
+import CoworkerStatus from '@/components/CoworkerStatus';
 
 const MainPage = () => {
   return (
     <>
       <Header />
       <TodoInputBar />
+      <CoworkerStatus />
     </>
   );
 };
