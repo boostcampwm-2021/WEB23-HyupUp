d4f1fc8b5ca5c2604b2d394eee209a2cc5302a6c
이찬호 <chanholee275@gmail.com>
2021-11-11 18:38:16 +0900

    layout: 할 일 입력창과 동료 상태 표시창 레이아웃 수정 [#36]
    입력창과 동료 상태 표시창이 겹쳐 보여 레이아웃 수정

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import Header from '@/lib/common/Header';
 import TodoInputBar from '@/components/TodoInputBar';
 import CoworkerStatus from '@/components/CoworkerStatus';
@@ -7,10 +8,22 @@ const MainPage = () => {
   return (
     <>
       <Header />
-      <TodoInputBar />
-      <CoworkerStatus />
+      <ContentContainer>
+        <TodoInputBar />
+        <CoworkerStatus />
+      </ContentContainer>
     </>
   );
 };
 
 export default MainPage;
+
+const ContentContainer = styled.div`
+  width: 1140px;
+  height: 883px;
+
+  margin-top: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
