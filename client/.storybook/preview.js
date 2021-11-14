2014bab2de98b5162cfbc9263a6de0a1addefb61
ingong <insong1216@gmail.com>
2021-11-15 00:47:42 +0900

    feat: storybook 설정 main, preview 파일 추가 [#71]
    - main.js : storybook 을 위한 config 설정이 담겨 있음
    - preview.js : 모든 Story 에 global 하게 적용될 포맷을 세팅함

---
+++ client/.storybook/preview.js
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components';
+import { addDecorator } from '@storybook/react';
+import { withThemes } from '@react-theming/storybook-addon';
+import theme from '../src/styles/theme';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+addDecorator(withThemes(ThemeProvider, [theme]));
