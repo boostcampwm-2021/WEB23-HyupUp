8c18b12cefd881b3f763a21945d3d1b279c1948c
ingong <insong1216@gmail.com>
2021-11-15 01:09:43 +0900

    fix: storyBook globalstyle 적용되지 않던 문제 해결 [#71]
    - addDecorator 활용법을 통해 globalstyle 적용함
    - 관련 링크 https://medium.com/strands-tech-corner/storybook-configuration-in-react-project-ec59869f3e7d

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
   stories: ['../src/stories/*.stories.mdx', '../src/stories/*.stories.@(ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@react-theming/storybook-addon',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
