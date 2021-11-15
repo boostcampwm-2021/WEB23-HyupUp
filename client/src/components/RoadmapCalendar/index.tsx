02377b810b6f32e70a96dc695c769596ded1534f
rudy3091 <grayblack313@gmail.com>
2021-11-15 21:47:04 +0900

    feat: RoadmapCalendar 컴포넌트 스타일 추가 [#18]
    간단한 ui 코드 작성

--- client/src/components/RoadmapCalendar/index.tsx
+++ client/src/components/RoadmapCalendar/index.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import S from '@/components/RoadmapCalendar/style';
 
 const RoadmapCalendar = () => {
-  return <S.RoadmapCalendar>content</S.RoadmapCalendar>;
+  const [date, setDate] = useState(new Date());
+  return (
+    <S.RoadmapCalendar>
+      <S.CalendarHead>{date.getMonth() + 1}월</S.CalendarHead>
+      <S.DaysWrapper>
+        {Array.from(new Array(14), (_, i) => (
+          // TODO: key props를 index 값이 아닌 date 값과 연관한 값으로 변경
+          <div key={i}>{i + 1}</div>
+        ))}
+      </S.DaysWrapper>
+    </S.RoadmapCalendar>
+  );
 };
 
 export default RoadmapCalendar;
--- client/src/components/RoadmapCalendar/style.ts
+++ client/src/components/RoadmapCalendar/style.ts
@@ -9,6 +9,14 @@ const S = {
     background-color: ${({ theme }) => theme.color.white};
     border-radius: 8px;
   `,
+  CalendarHead: styled.h3`
+    margin-bottom: 24px;
+    font: ${({ theme }) => theme.font.bold_medium};
+  `,
+  DaysWrapper: styled.div`
+    display: flex;
+    justify-content: space-between;
+  `,
 };
 
 export default S;
