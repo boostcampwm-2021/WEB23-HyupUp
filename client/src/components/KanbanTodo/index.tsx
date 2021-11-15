fc43ec635a9d2d79777376aec201810a1386ca89
ingong <insong1216@gmail.com>
2021-11-15 17:55:07 +0900

    refactor: 불필요 Type 선언 및 함수 제거 [#83]
    - Type 선언
    - api 호출 함수를 WorkPage 로 이동시켰기 때문에 해당 함수 제거

--- client/src/components/KanbanTodo/index.tsx
+++ client/src/components/KanbanTodo/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { useStoryState, useStoryDispatch } from '@/lib/hooks/useContextHooks';
-import { createStory, getAllStories } from '@/lib/api/story';
+import { createStory } from '@/lib/api/story';
 import useInput from '@/lib/hooks/useInput';
 import Styled from '@/components/KanbanTodo/style';
 import Button from '@/lib/design/Button';
-import { StoryType, StatusType } from '@/types/story';
-
-interface StoryObject {
-  id: number;
-  name: string;
-  status: StatusType;
-}
+import { StoryType } from '@/types/story';
 
 const KanbanTodo = () => {
   const storyArray = useStoryState();
@@ -18,7 +12,7 @@ const KanbanTodo = () => {
 
   const [value, onChange] = useInput();
   const lastStoryId = storyArray.length > 0 ? storyArray[storyArray.length - 1]?.id : 0;
-  const StoryObject: StoryObject = {
+  const StoryObject: StoryType = {
     id: lastStoryId + 1,
     name: '',
     status: 'TODO',
@@ -29,7 +23,6 @@ const KanbanTodo = () => {
 
     //todo 현재 CurrentProjectId 와 EPIC ID 를 하드코딩함
     // CurrentProjectID 는 세션으로 유저 정보 조회 후 가져올 예정
-    // EPIC ID 는 DB 에 저장 시 undefined 로 저장이 가능한지 협의 후 수정
     createStory(StoryObject.id, StoryObject.status, 1, StoryObject.name, 1);
     // createStory(StoryObject.id, StoryObject.status, userState.currentProjectId as number, '', '');
   };
