a4d96db6b53c9a5d65dd04acd4bb4269452bdfca
이찬호 <chanholee275@gmail.com>
2021-11-15 20:36:34 +0900

    refactor: cors option을 app에서 주기 때문에 router cors 제거

--- server/src/Epics/Epics.router.ts
+++ server/src/Epics/Epics.router.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-import cors from 'cors';
-import options from '../../lib/config/corsConfig';
 import { createEpic, findEpicById, getAllEpicsByProject } from './Epics.controller';
 
 const router = express.Router();
 
-router.get('/', cors(options), getAllEpicsByProject);
-router.get('/:id', cors(options), findEpicById);
+router.get('/', getAllEpicsByProject);
+router.get('/:id', findEpicById);
 router.post('/', createEpic);
 
 export default router;
