23964254c06a91abbf38a625afe23ff08f1992fa
이찬호 <chanholee275@gmail.com>
2021-11-15 21:57:21 +0900

    feat: 스토리 id를 가지고 task를 검색하는 API 작성
    story를 가지고 task를 보여주기 위해서 task를 검색하는 API 작성

--- server/src/Tasks/Tasks.router.ts
+++ server/src/Tasks/Tasks.router.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getAllTasksByProject } from './Tasks.controller';
+import { getAllTasksByProject, getTasksByStoryId } from './Tasks.controller';
 
 const router = express.Router();
 
 router.get('/', getAllTasksByProject);
+
+router.get('/:id', getTasksByStoryId);
 
 export default router;
