112fb7d304d506e20611b4577a13688bbc1c9961
ingong <insong1216@gmail.com>
2021-11-15 23:23:27 +0900

    feat: X 버튼 클릭 시 모달이 나타난다 [#85]
    - X 버튼 클릭 시 칸반 아이템 삭제 여부를 묻는 모달이 보인다

--- client/src/components/KanbanTodo/index.tsx
+++ client/src/components/KanbanTodo/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStoryState, useStoryDispatch } from '@/lib/hooks/useContextHooks';
 import { createStory, updateStoryWithName } from '@/lib/api/story';
 import useInput from '@/lib/hooks/useInput';
 import Styled from '@/components/KanbanTodo/style';
 import Button from '@/lib/design/Button';
+import Modal from '@/lib/design/Modal';
 import { StoryType } from '@/types/story';
 
 interface KanbanProps {
@@ -14,6 +15,7 @@ const KanbanTodo = ({ projectId }: KanbanProps) => {
   const storyArray = useStoryState();
   const useDispatch = useStoryDispatch();
   const [key, value, handleChange] = useInput();
+  const [showModal, setShowModal] = useState(false);
 
   const lastStoryId = storyArray.length > 0 ? storyArray[storyArray.length - 1]?.id : 0;
   const StoryObject: StoryType = {
@@ -35,7 +37,6 @@ const KanbanTodo = ({ projectId }: KanbanProps) => {
   return (
     <Styled.Column>
       <h4>To do</h4>
-
       {storyArray?.map((story) => {
         return (
           <Styled.KanBanItem key={story.id}>
@@ -46,13 +47,21 @@ const KanbanTodo = ({ projectId }: KanbanProps) => {
               onChange={handleChange}
               onBlur={useUpdateStory}
             />
-            <Styled.CancelIcon />
+            <Styled.CancelIcon onClick={() => setShowModal(true)}></Styled.CancelIcon>
           </Styled.KanBanItem>
         );
       })}
       <Button size={'large'} category={'cancel'} onClick={useAddStory}>
         Add Todo
       </Button>
+      <Modal
+        shouldConfirm
+        title={'스토리를 삭제하시겠습니까?'}
+        visible={showModal}
+        onClickCancel={() => console.log('cancel!')}
+        onClickOk={() => console.log('ok!')}
+        onClose={() => setShowModal(false)}
+      ></Modal>
     </Styled.Column>
   );
 };
