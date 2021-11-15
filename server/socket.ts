edd240ed76ad432063c261bff05057dd302f3fe8
이찬호 <chanholee275@gmail.com>
2021-11-15 18:53:07 +0900

    feat: socket 연결이 끊어질 때, 로그아웃으로 표시 되도록 기능 추가 [#38]
    로그아웃 버튼을 누르지 않고 나갔을 경우, 로그 아웃으로 처리

--- server/socket.ts
+++ server/socket.ts
@@ -2,8 +2,14 @@ import { Server } from 'socket.io';
 
 const io = new Server();
 
+interface userInstance {
+  sid: string;
+  userId: number;
+}
+
 // to-do 메모리 상에서 관리하는 것 말고, redis나 다른 DB 생각해보기
-const usersList: { [index: string]: number[] } = {
+// to-do room을 활용하여 관리하는 방법을 찾아보기
+const usersList: { [index: string]: userInstance[] } = {
   TEAM42: [],
 };
 
@@ -13,12 +19,23 @@ io.on('connection', (socket) => {
   socket.on('LOGIN', (userId: number) => {
     socket.to('TEAM42').emit('ON', userId);
     io.to(socket.id).emit('LOGIN_CALLBACK', usersList.TEAM42);
-    usersList.TEAM42 = [...usersList.TEAM42, userId];
+    usersList.TEAM42 = [...usersList.TEAM42, { userId: userId, sid: socket.id }];
   });
 
   socket.on('LOGOUT', (userId: number) => {
     socket.to('TEAM42').emit('OFF', userId);
-    usersList.TEAM42 = [...usersList.TEAM42.filter((el) => el !== userId)];
+    usersList.TEAM42 = [
+      ...usersList.TEAM42.filter((el) => el.userId !== userId).map((el) => ({ ...el })),
+    ];
+  });
+
+  socket.on('disconnecting', () => {
+    const id = usersList.TEAM42.filter((el) => el.sid === socket.id);
+    if (id.length === 0) return;
+    usersList.TEAM42 = [
+      ...usersList.TEAM42.filter((el) => el.sid !== socket.id).map((el) => ({ ...el })),
+    ];
+    socket.to('TEAM42').emit('OFF', id[0].userId);
   });
 });
 
