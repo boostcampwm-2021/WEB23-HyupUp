74402ddf1a15b70eb5f8ec4e0485b130059844f6
이찬호 <chanholee275@gmail.com>
2021-11-16 13:27:41 +0900

    refactor: 변수명을 알아보기 쉽게 변경

--- client/src/layers/CoworkerStatus/index.tsx
+++ client/src/layers/CoworkerStatus/index.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { getUsersByOrganization, UserProfile } from '@/lib/api/user';
 import { useUserState } from '@/lib/hooks/useContextHooks';
 import useSocketSend from '@/lib/hooks/useSocketSend';
 import useSocketReceive from '@/lib/hooks/useSocketReceive';
 
-import Avator from '@/components/CoworkerStatusItem/Avator';
+import Avatar from '@/components/CoworkerStatusItem/Avatar';
 import * as S from './style';
 import StatusTitle from '@/components/CoworkerStatusItem/StatusTitle';
 
@@ -15,59 +13,62 @@ interface UserStatus extends UserProfile {
   status: boolean;
 }
 
-interface userSocketInstance {
+interface UserSocketInstance {
   sid: string;
   userId: number;
 }
 
 const CoworkerStatus = () => {
-  const [usersList, usersListHandler] = useState<Array<UserStatus>>([]);
-  const [usersIdList, usersIdListHandler] = useState<Array<number>>([]);
-  const [users, usersHandler] = useState<Array<UserProfile>>([]);
-  const socketSend = useSocketSend('LOGIN');
+  const [usersList, setUsersList] = useState<Array<UserStatus>>([]);
+  const [usersIdList, setUsersIdList] = useState<Array<number>>([]);
+  const [users, setUsers] = useState<Array<UserProfile>>([]);
+  const emitLoginEvent = useSocketSend('LOGIN');
   const userState = useUserState();
 
-  useSocketReceive('LOGIN_CALLBACK', (userIds: Array<userSocketInstance>) => {
+  useSocketReceive('LOGIN_CALLBACK', (userIds: Array<UserSocketInstance>) => {
     const ids = userIds.map((el) => el.userId);
-    usersIdListHandler(ids);
+    setUsersIdList(ids);
   });
 
   useSocketReceive('ON', (userId: number) => {
-    usersIdListHandler([...usersIdList, userId]);
+    setUsersIdList([...usersIdList, userId]);
   });
 
   useSocketReceive('OFF', (userId: number) => {
-    usersIdListHandler(usersIdList.filter((el) => el !== userId));
+    setUsersIdList(usersIdList.filter((el) => el !== userId));
   });
 
   // todo error 발생 원인 찾기
   useEffect(() => {
-    socketSend(userState.id as number);
+    emitLoginEvent(userState.id as number);
   }, [userState]);
 
   useEffect(() => {
     (async () => {
       const users = await getUsersByOrganization(userState.organization as number);
-      usersHandler([...users]);
+      setUsers([...users]);
     })();
   }, [userState.organization]);
 
   useEffect(() => {
     const usersWithOutMe = users.filter((el) => el.index !== userState.id);
-    const updateUsers = usersWithOutMe.map((el) => {
-      if (usersIdList.includes(el.index))
-        return {
-          ...el,
-          status: true,
-        };
-      return {
-        ...el,
-        status: false,
-      };
-    });
+    const updateUsers = usersWithOutMe.map((el) => ({
+      ...el,
+      status: usersIdList.includes(el.index),
+    }));
+
     const logInUsers = updateUsers.filter((el) => el.status === true);
     const logOutUsers = updateUsers.filter((el) => el.status === false);
-    usersListHandler([...logInUsers, ...logOutUsers]);
+
+    const sorted = updateUsers.sort((a, b) => {
+      if (a.status && !b.status) return -1;
+      if (!a.status && b.status) return 1;
+      return -1;
+    });
+
+    console.log(sorted);
+
+    setUsersList([...logInUsers, ...logOutUsers]);
   }, [userState.id, users, usersIdList]);
 
   return (
@@ -76,7 +77,7 @@ const CoworkerStatus = () => {
       <S.UsersContainer>
         {usersList.map((el) => (
           <S.StatusContainer key={el.index}>
-            <Avator src={el.imageURL} status={el.status} />
+            <Avatar src={el.imageURL} status={el.status} />
             <S.Name>{el.name}</S.Name>
           </S.StatusContainer>
         ))}
