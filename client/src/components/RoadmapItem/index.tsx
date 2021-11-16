bab6b9498bd555f621f420e7ef6610996dfea0d9
rudy3091 <grayblack313@gmail.com>
2021-11-16 21:52:00 +0900

    feat: RoadmapItem 컴포넌트 추가 [#22]
    로드맵 항목의 막대 역할을 하는 컴포넌트

---
+++ client/src/components/RoadmapItem/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import S from './style';
+
+const RoadmapItem = () => {
+  return <S.Container />;
+};
+
+export default RoadmapItem;
---
+++ client/src/components/RoadmapItem/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const S = {
+  Container: styled.div`
+    width: 48px;
+    height: 24px;
+
+    background-color: ${({ theme }) => theme.color.blue300};
+    border-radius: 8px;
+  `,
+};
+
+export default S;
