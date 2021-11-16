38bae0f038db82bc67d7dad8fc4e53f762a0b4f2
이찬호 <chanholee275@gmail.com>
2021-11-16 18:00:09 +0900

    refactor: task GET 요청 실패 시, 사용자에게 알리는 기능 수정

--- client/src/lib/common/message/error.ts
+++ client/src/lib/common/message/error.ts
@@ -2,6 +2,7 @@ export const GET_USER = '유저 정보 요청에 실패하였습니다.';
 export const GET_EPIC = '에픽 정보 요청에 실패했습니다.';
 export const GET_PROJECT = '프로젝트 정보 요청에 실패했습니다.';
 export const GET_STORY = '스토리 정보 요청에 실패했습니다.';
+export const GET_TASK = '테스크 정보 요청에 실패했습니다.';
 
 export const CREATE_EPIC = '에픽 생성에 실패했습니다.';
 export const CREATE_TODO = 'Todo 생성에 실패했습니다.';
@@ -11,6 +12,7 @@ export default {
   GET_EPIC,
   GET_PROJECT,
   GET_STORY,
+  GET_TASK,
   CREATE_EPIC,
   CREATE_TODO,
 };
