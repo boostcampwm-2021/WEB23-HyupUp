6f03d0c2c4744a7b691d80b6d6a2cd7520650003
ingong <insong1216@gmail.com>
2021-11-16 14:45:37 +0900

    feat: layers, components index 파일에 export default 추가
    - 여러 파일을 import 하면서 import 를 하는 라인이 많아졌기 때문에

---
+++ client/src/layers/index.ts
@@ -0,0 +1,9 @@
+// MainPage
+export { default as Header } from './Header/index';
+export { default as SideBar } from './SideBar/index';
+export { default as ListView } from './ListView/index';
+
+// WorkPage
+export { default as Roadmap } from './Roadmap/index';
+export { default as Kanban } from './Kanban/index';
+export { default as Backlog } from './Backlog/index';
