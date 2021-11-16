af43518c8618455835858b2266cd5f31fd0e372f
이찬호 <chanholee275@gmail.com>
2021-11-16 17:16:41 +0900

    feat: 테스크를 보여주는 컴포넌트를 화면에 적용 [#82]
    테스크를 보여주는 컴포넌트를 디자인에 맞게 보여주기 위하여 스토리를 보여주는 컴포넌트에 테스크를 보여주는 컴포넌트 추가

--- client/src/components/BackLogItem/index.tsx
+++ client/src/components/BackLogItem/index.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import S from './style';
 import arrow from '@public/icons/chevron-down.svg';
-import { useState } from 'react';
+import BackLogTask from '../BackLogTask';
+import { getTasksByStoryId } from '@/lib/api/task';
+import { TaskProps } from '@/lib/api/task';
 
-const BackLogItem = ({ name }: { name: string }) => {
+const BackLogItem = ({ name, id }: { name: string; id: number }) => {
   const [isClick, isClickHandler] = useState(false);
-  const clickEventListener = () => {
+  const [tasks, setTasks] = useState<Array<TaskProps>>([]);
+  const clickEventListener = async () => {
     isClickHandler(!isClick);
+    if (isClick === true) return;
+    setTasks(await getTasksByStoryId(id));
   };
   return (
-    <S.ItemContainer>
-      <S.StoryText>{name}</S.StoryText>
-      <S.ToggleButton onClick={clickEventListener}>
-        <S.ToggleImg src={arrow} check={isClick} />
-      </S.ToggleButton>
-    </S.ItemContainer>
+    <div>
+      <S.ItemContainer>
+        <S.StoryText>{name}</S.StoryText>
+        <S.ToggleButton onClick={clickEventListener}>
+          <S.ToggleImg src={arrow} check={isClick} />
+        </S.ToggleButton>
+      </S.ItemContainer>
+      <S.TaskContainer check={isClick}>
+        {tasks.map((el) => (
+          <BackLogTask key={el.id} name={el.user} imageURL={el.userImage} task={el.name} />
+        ))}
+      </S.TaskContainer>
+    </div>
   );
 };
 
