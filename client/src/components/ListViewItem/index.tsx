a991220124293b7427abd4426f214375ff7586b1
Harry <haesoo9410@gmail.com>
2021-11-16 21:32:32 +0900

    layout: 리스트 아이템 타이틀에 아이콘 추가 개인과 프로젝트를 구분하는 아이콘 추가 개인은 PERSONAL, 프로젝트는 PROJECT 표시

--- client/src/components/ListViewItem/index.tsx
+++ client/src/components/ListViewItem/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import projectIcon from '@public/icons/project-icon.svg';
+import personalIcon from '@public/icons/personal-icon.svg';
 import Styled from '@/components/ListViewItem/style';
 import { TaskProp } from '@/layers/ListView';
 import Button from '@/lib/design/Button';
@@ -9,9 +11,8 @@ interface ListItemProp {
 }
 
 const ListViewItem = ({ task, onClickMethod }: ListItemProp) => {
-  const buttonComponent = () => {
+  const buttonComponent = useMemo(() => {
     if (!task.status) {
-      // 할 일
       return (
         <Button size="small" category="default" onClick={() => onClickMethod(task)}>
           완료
@@ -24,13 +25,16 @@ const ListViewItem = ({ task, onClickMethod }: ListItemProp) => {
         </Button>
       );
     }
-  };
+  }, [task, onClickMethod]);
 
   return (
     <Styled.Container>
-      <Styled.Title>{task.project ? task.project.name : 'TODO'}</Styled.Title>
+      <Styled.Title>
+        <img src={task.project ? projectIcon : personalIcon} />
+        <h3>{task.project ? 'PROJECT' : 'PERSONAL'}</h3>
+      </Styled.Title>
       <Styled.Content>{task.name}</Styled.Content>
-      {buttonComponent()}
+      {buttonComponent}
     </Styled.Container>
   );
 };
