ceaed22211ce98d01c387b72f80ded15459c559b
rudy3091 <grayblack313@gmail.com>
2021-11-17 21:21:32 +0900

    feat: 유틸함수 추가 [#22]
    범위판별을 하여 달력 뷰에 표시해야하는지를 알아내기 위한 유틸함수 추가

--- client/src/lib/utils/date.ts
+++ client/src/lib/utils/date.ts
@@ -50,3 +50,53 @@ export const getRangeFromDate = (date: Date) => {
     to: new Date(year, month, day + REAR_HALF),
   };
 };
+
+/**
+ *
+ * @param target date 보다 앞에있는지 확인할 date 객체
+ * @param date 확인의 기준이 되는 date 객체
+ */
+export const isFormer = (target: Date, date: Date): boolean => {
+  const targetYMD = getYMD(target);
+  const dateYMD = getYMD(date);
+  return (
+    targetYMD.year < dateYMD.year || targetYMD.month < dateYMD.month || targetYMD.day < dateYMD.day
+  );
+};
+
+/**
+ *
+ * @param target date 보다 뒤에있는지 확인할 date 객체
+ * @param date 확인의 기준이 되는 date 객체
+ */
+export const isLatter = (target: Date, date: Date): boolean => {
+  const targetYMD = getYMD(target);
+  const dateYMD = getYMD(date);
+  return (
+    targetYMD.year > dateYMD.year || targetYMD.month > dateYMD.month || targetYMD.day > dateYMD.day
+  );
+};
+
+/**
+ *
+ * @param param.start 범위 내에 있는지 확인할 범위의 왼쪽 끝에 해당하는 날짜의 date 객체
+ * @param param.end 범위 내에 있는지 확인할 범위의 오른쪽 끝에 해당하는 날짜의 date 객체
+ * @param param.from 확인할 범위의 왼쪽 끝에 해당하는 날짜의 date 객체
+ * @param param.to 확인할 범위의 오른쪽 끝에 해당하는 날짜의 date 객체
+ * @return start - end 범위가 from - to 범위와 겹치는 부분이 있다면 true, 아니라면 false 를 반환
+ */
+export const shouldRender = ({
+  start,
+  end,
+  from,
+  to,
+}: {
+  start: Date;
+  end: Date;
+  from: Date;
+  to: Date;
+}) => {
+  if (isFormer(end, from)) return false;
+  else if (isLatter(start, to)) return false;
+  return true;
+};
