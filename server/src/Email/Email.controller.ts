b13d39b96ac9c1850ffbf49246323eb7057b9b74
이찬호 <chanholee275@gmail.com>
2021-11-17 21:55:12 +0900

    feat: JWT 생성 로직 작성 [#112]
    조직의 정보를 보내기 위하여 이메일 사용자에게 보낼 JWT 생성

--- server/src/Email/Email.controller.ts
+++ server/src/Email/Email.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { bodyValidator } from 'lib/utils/requestValidator';
 import { sendMail } from './Email.service';
 
@@ -7,9 +8,13 @@ export const inviteByEmail = (req: Request, res: Response) => {
     if (!bodyValidator(req.body, ['email', 'organizationId'])) {
       throw Error('body is not valid');
     }
-    // to-do organizationId과 email 포함한 JWT 발급 코드 작성
-    // JWT를 저장
-    sendMail(req.body.email, 'testjwt');
+    const secret = process.env.SECRET as string;
+    const token = jwt.sign({ id: req.body.organizationId, email: req.body.email }, secret, {
+      algorithm: 'HS256',
+      expiresIn: '1h',
+    });
+    // to-do JWT를 저장
+    sendMail(req.body.email, token);
     res.status(201).end();
   } catch (e) {
     res.status(400).json({ message: (e as Error).message });
