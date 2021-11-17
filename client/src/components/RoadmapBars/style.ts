778204e80707f1abb9678e63a1bf7f2e54f46586
rudy3091 <grayblack313@gmail.com>
2021-11-17 17:16:28 +0900

    feat: 로드맵 스타일을 위해 막대 여러개 추가 [#22]
    로드맵 막대의 간격을 맞추기 위해 테스트로 여러개 추가해놓음

--- client/src/components/RoadmapBars/style.ts
+++ client/src/components/RoadmapBars/style.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 const S = {
-  Container: styled.div``,
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
 };
 
 export default S;
