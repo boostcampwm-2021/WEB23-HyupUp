eb4407f4ee8152745ba5d9d9ef9298504aab91ac
rudy3091 <grayblack313@gmail.com>
2021-11-17 17:28:00 +0900

    layout: 로드맵 막대 스타일 수치 조정 [#22]
    왼쪽 영역의 에픽 항목들과 크기를 맞추기 위해 수치를 조정함

--- client/src/components/RoadmapItem/style.ts
+++ client/src/components/RoadmapItem/style.ts
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 const S = {
   Container: styled.div`
-    width: 48px;
-    height: 24px;
-    margin: 27px;
+    width: 40px;
+    height: 23px;
+    margin: 25px 0;
 
     background-color: ${({ theme }) => theme.color.blue300};
     border-radius: 8px;
