7bdfc3ba787f0125b8ae1d33b0ecf191636e88ce
rudy3091 <grayblack313@gmail.com>
2021-11-19 01:09:37 +0900

    feat: 드래그 시작 핸들러 전달 [#26]
    RoadmapItem 컴포넌트에 handle 엘리먼트를 드래그하기 시작할 때 실행할
    이벤트 핸들러를 전달함

--- client/src/components/RoadmapItem/index.tsx
+++ client/src/components/RoadmapItem/index.tsx
@@ -7,22 +7,38 @@ interface RoadmapItemProps {
   length: number;
   exceedsLeft: boolean;
   exceedsRight: boolean;
+  handleDragStart: () => void;
 }
 
-const RoadmapItem = ({ columns, index, length, exceedsLeft, exceedsRight }: RoadmapItemProps) => {
+const RoadmapItem = ({
+  columns,
+  index,
+  length,
+  exceedsLeft,
+  exceedsRight,
+  handleDragStart,
+}: RoadmapItemProps) => {
   return (
     <S.Container columns={columns}>
       {[...Array(columns)].map((_, i) => {
         if (i === index)
           return (
             <S.Bar key={i}>
-              {exceedsLeft ? undefined : <S.FrontHandle draggable="true" />}
-              {length === 0 && !exceedsRight ? <S.RearHandle draggable="true" /> : undefined}
+              {exceedsLeft ? undefined : (
+                <S.FrontHandle draggable="true" onDragStart={handleDragStart} />
+              )}
+              {length === 0 && !exceedsRight ? (
+                <S.RearHandle draggable="true" onDragStart={handleDragStart} />
+              ) : undefined}
             </S.Bar>
           );
         else if (i === index + length)
           return (
-            <S.Bar key={i}>{exceedsRight ? undefined : <S.RearHandle draggable="true" />}</S.Bar>
+            <S.Bar key={i}>
+              {exceedsRight ? undefined : (
+                <S.RearHandle draggable="true" onDragStart={handleDragStart} />
+              )}
+            </S.Bar>
           );
         else if (i < index + length && i > index) return <S.Bar key={i} />;
         else return <S.Spacer key={i} />;
