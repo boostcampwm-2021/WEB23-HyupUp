57744e01d9981d84d520a19972708debc4a13025
이찬호 <chanholee275@gmail.com>
2021-11-18 21:04:08 +0900

    feat: 팀원 관리 페이지 작성 [#122]
    더미 데이터를 보여주기 위한 팀원 관리 페이지 작성

--- client/src/layers/GroupManagement/index.tsx
+++ client/src/layers/GroupManagement/index.tsx
@@ -1,5 +1,43 @@
 import React from 'react';
+import * as S from './style';
+import { DropDown } from '@/lib/design';
+import dots from '@public/icons/more_horiz.svg';
+import TeamManagementItem from '@/components/TeamManagementItem';
+
+const dropDownList = [
+  {
+    id: 1,
+    name: '팀원에서 제외',
+  },
+  {
+    id: 2,
+    name: '관리자로 변경',
+  },
+];
 
 export const GroupManagement = () => {
-  return <div>그룹 관리 페이지</div>;
+  return (
+    <S.Container>
+      <S.ItemListViewer>
+        <TeamManagementItem
+          imageURL="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+          name="이찬호"
+          job="개발자"
+          admin={true}
+          dropDown={
+            <DropDown Title={<img src={dots} />} list={dropDownList} handleClick={(e) => e} />
+          }
+        />
+        <TeamManagementItem
+          imageURL="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+          name="이찬호"
+          job="BIZ HR"
+          admin={true}
+          dropDown={
+            <DropDown Title={<img src={dots} />} list={dropDownList} handleClick={(e) => e} />
+          }
+        />
+      </S.ItemListViewer>
+    </S.Container>
+  );
 };
