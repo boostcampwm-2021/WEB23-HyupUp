3f67a42c1aef6dc194befc0779d22fd6519d1cc4
rudy3091 <grayblack313@gmail.com>
2021-11-18 22:22:40 +0900

    fix: 캘린더 뷰의 막대 front handle 로직 수정 [#22]
    캘린더 뷰의 범위를 왼쪽으로 초과하는 에픽에 대해 막대의 왼쪽 핸들이
    렌더링되지 않도록 수정

--- client/src/components/RoadmapItem/index.tsx
+++ client/src/components/RoadmapItem/index.tsx
@@ -5,17 +5,19 @@ interface RoadmapItemProps {
   columns: number;
   index: number;
   length: number;
+  exceedsLeft: boolean;
+  exceedsRight: boolean;
 }
 
-const RoadmapItem = ({ columns, index, length }: RoadmapItemProps) => {
+const RoadmapItem = ({ columns, index, length, exceedsLeft, exceedsRight }: RoadmapItemProps) => {
   return (
     <S.Container columns={columns}>
       {[...Array(columns)].map((_, i) => {
         if (i === index)
           return (
             <S.Bar key={i}>
-              <S.FrontHandle />
-              {length === 0 ? <S.RearHandle /> : undefined}
+              {exceedsLeft ? undefined : <S.FrontHandle />}
+              {length === 0 && !exceedsRight ? <S.RearHandle /> : undefined}
             </S.Bar>
           );
         else if (i === index + length)
