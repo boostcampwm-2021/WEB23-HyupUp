fe31464fe73224bb77f1ea15d58b4e580d11dbd1
ingong <insong1216@gmail.com>
2021-11-19 00:45:04 +0900

    fix: useEffect Dependency 에러 해결을 위한 로직 및 AddBtn 으로 역할 분리 [#87]
    - 해당 기능을 KanBanAddBtn 으로 이동

--- client/src/components/KanbanAddBtn/index.tsx
+++ client/src/components/KanbanAddBtn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createStory } from '@/lib/api/story';
 import { Button } from '@/lib/design';
 import { useStoryDispatch, useStoryState, useUserState } from '@/lib/hooks/useContextHooks';
@@ -14,11 +14,36 @@ const KanbanAddBtn = () => {
   const storyList = useStoryState();
   const dispatchStory = useStoryDispatch();
   const lastStoryId = extractLastID(storyList);
-  const initStoryData = { id: lastStoryId + 1, name: '', status: 'TODO', order: 1 };
+  const todoList = storyList.filter((item) => item.status === 'TODO');
+  const [isInitial, setIsInitial] = useState(true);
+  const initialItem = {
+    id: lastStoryId + 1,
+    name: '',
+    status: 'TODO',
+    order: 1,
+  };
+
+  useEffect(() => {
+    if (isInitial && todoList.length > 1) {
+      if (todoList.length === 2) {
+        dispatchStory({ type: 'UPDATE_STORY', story: { ...todoList[0], order: 0 } });
+      } else {
+        dispatchStory({
+          type: 'UPDATE_STORY',
+          story: {
+            ...todoList[todoList.length - 2],
+            order: (todoList[todoList.length - 3]?.order + 1) / 2,
+          },
+        });
+      }
+      setIsInitial((prev) => !prev);
+    }
+  }, [todoList, dispatchStory, isInitial]);
 
   const addStory = () => {
-    dispatchStory({ type: 'ADD_STORY', story: initStoryData });
-    createStory({ ...initStoryData, projectId: userState.currentProjectId });
+    dispatchStory({ type: 'ADD_STORY', story: initialItem });
+    createStory({ ...initialItem, projectId: userState.currentProjectId });
+    setIsInitial((prev) => !prev);
   };
 
   return (
