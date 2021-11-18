be66e488c6a5af4ddccbc2962b751616592e533b
Harry <haesoo9410@gmail.com>
2021-11-18 16:07:43 +0900

    feat: 필요한 selector 추가 [#114]

--- client/src/recoil/user/selector.ts
+++ client/src/recoil/user/selector.ts
@@ -1,29 +1,80 @@
-import { selector, selectorFamily } from 'recoil';
-import userState from '@/recoil/user/atom';
-import { PrivateTask } from '@/types/task';
+import { DefaultValue, selector, selectorFamily } from 'recoil';
+import produce from 'immer';
+import userAtom from '@/recoil/user/atom';
+import { PrivateTask, ProjectTask } from '@/types/task';
 
-export const privateTasksSelector = selector({
-  key: 'privateTasks',
+export const privateTasksSelector = selector<PrivateTask[]>({
+  key: 'privateTasksSelector',
   get: ({ get }) => {
-    const user = get(userState);
+    const user = get(userAtom);
     return user.privateTasks ? user.privateTasks : [];
+  },
+  set: ({ set }, newValue) => {
+    set(
+      userAtom,
+      newValue instanceof DefaultValue
+        ? newValue
+        : (prev) =>
+            produce(prev, (draft) => {
+              draft.privateTasks = [...newValue];
+              return draft;
+            }),
+    );
   },
 });
 
-export const privateTaskWithIdSelector = selectorFamily({
-  key: 'privateTask',
+export const projectTasksSelector = selector<ProjectTask[]>({
+  key: 'projectTasksSelector',
+  get: ({ get }) => {
+    const user = get(userAtom);
+    return user.projectTasks ? user.projectTasks : [];
+  },
+  set: ({ set }, newValue) => {
+    set(
+      userAtom,
+      newValue instanceof DefaultValue
+        ? newValue
+        : (prev) =>
+            produce(prev, (draft) => {
+              draft.projectTasks = [...newValue];
+              return draft;
+            }),
+    );
+  },
+});
+
+export const allTasksSelector = selector<ProjectTask[] | PrivateTask[]>({
+  key: 'allTasksSelector',
+  get: ({ get }) => {
+    const privateTasks = get(privateTasksSelector);
+    const projectTasks = get(projectTasksSelector);
+    return [...privateTasks, ...projectTasks];
+  },
+});
+
+export const privateTaskWithIdSelector = selectorFamily<PrivateTask | undefined, number>({
+  key: 'privateTaskWithIdSelector',
   get:
     (id) =>
     ({ get }) => {
       const tasks = get(privateTasksSelector);
       return tasks?.find((el) => el.id === id);
     },
-});
-
-export const projectTasksSelector = selector({
-  key: 'projectTasks',
-  get: ({ get }) => {
-    const user = get(userState);
-    return user.projectTasks ? user.projectTasks : [];
-  },
+  set:
+    (id: number) =>
+    ({ set, get }, newValue) => {
+      set(
+        privateTasksSelector,
+        newValue instanceof DefaultValue
+          ? [...get(privateTasksSelector)]
+          : (prev) =>
+              produce(prev, (draft) => {
+                const newTask = draft.find((el) => el.id === id);
+                if (!newTask) return draft;
+                newTask.name = newValue!.name;
+                newTask.status = newValue!.status;
+                return draft;
+              }),
+      );
+    },
 });
