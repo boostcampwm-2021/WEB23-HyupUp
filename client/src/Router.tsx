9635af31b75e82205bfa7cd892af0849ac76b112
이찬호 <chanholee275@gmail.com>
2021-11-18 15:39:18 +0900

    feat: 클라이언트 라우터에 setting 페이지 추가 [#118]
    아이콘을 작동하게 하기 위해서 setting 페이지를 라우터에 추가

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -5,6 +5,7 @@ import MainPage from './pages/MainPage';
 import WorkPage from './pages/WorkPage';
 import { useRecoilValue } from 'recoil';
 import user from '@/recoil/user';
+import AdminPage from './pages/AdminPage';
 
 const Router = () => {
   const userState = useRecoilValue(user);
@@ -13,6 +14,7 @@ const Router = () => {
       <Switch>
         <Route exact path="/" render={() => (userState?.email ? <MainPage /> : <LandingPage />)} />
         <Route exact path="/work" render={() => <WorkPage />} />
+        <Route exact path="/setting" render={() => <AdminPage />} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
