55b15c92a74f4ae2da50b0e6bad8cbcad0c10193
rudy3091 <grayblack313@gmail.com>
2021-11-18 23:23:33 +0900

    layout: 에픽 막대 스타일링 수정 [#22]
    에픽 막대의 양 끝에 border-radius 속성을 추가함
    중간 영역의 막대 엘리먼트에는 둥근 모서리를 주지 않고 양 끝에만 주기
    위해 pseudo element ::after 를 사용

--- client/src/components/RoadmapItem/style.ts
+++ client/src/components/RoadmapItem/style.ts
@@ -23,21 +23,44 @@ const S = {
     background-color: ${({ theme }) => theme.color.blue300};
   `,
   FrontHandle: styled.div`
+    display: flex;
+
     width: ${HANDLE_WIDTH};
     height: 100%;
 
+    background-color: ${({ theme }) => theme.color.white};
     cursor: col-resize;
     z-index: 9;
+
+    &:after {
+      content: '';
+      width: 100%;
+      height: 100%;
+
+      border-radius: 8px 0 0 8px;
+      background-color: ${({ theme }) => theme.color.blue300};
+    }
   `,
   RearHandle: styled.div`
+    display: flex;
     position: absolute;
     right: 0px;
 
     width: ${HANDLE_WIDTH};
     height: 100%;
 
+    background-color: ${({ theme }) => theme.color.white};
     cursor: col-resize;
     z-index: 9;
+
+    &:after {
+      content: '';
+      width: 100%;
+      height: 100%;
+
+      border-radius: 0 8px 8px 0;
+      background-color: ${({ theme }) => theme.color.blue300};
+    }
   `,
 };
 
