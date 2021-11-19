d6c05c4b0c5cd788c327ff34aa1ed9e1fa1f1ad1
rudy3091 <grayblack313@gmail.com>
2021-11-19 21:48:11 +0900

    feat: 에픽 수정 라우팅 경로 추가 [#26]
    에픽을 수정하기 위한 요청을 받아들일 라우팅 경로 추가

--- server/src/Epics/Epics.router.ts
+++ server/src/Epics/Epics.router.ts
@@ -1,10 +1,17 @@
 import express from 'express';
-import { createEpic, deleteEpicById, findEpicById, getAllEpicsByProject } from './Epics.controller';
+import {
+  createEpic,
+  deleteEpicById,
+  findEpicById,
+  getAllEpicsByProject,
+  updateEpicById,
+} from './Epics.controller';
 
 const router = express.Router();
 
 router.get('/', getAllEpicsByProject);
 router.get('/:id', findEpicById);
+router.patch('/:id', updateEpicById);
 router.delete('/:id', deleteEpicById);
 router.post('/', createEpic);
 
