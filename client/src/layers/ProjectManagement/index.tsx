5513dfb7f4e591aa61ab50734b625ca170b6e76b
Harry <haesoo9410@gmail.com>
2021-11-22 23:32:23 +0900

    feat: 프로젝트 조회 api를 컴포넌트에 연결 [#135] 조회가 반영이안되므로 수정 필요

--- client/src/layers/ProjectManagement/index.tsx
+++ client/src/layers/ProjectManagement/index.tsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import userAtom from '@/recoil/user';
+import { createProject, getAllOrgProjects } from '@/lib/api/project';
+import { useInput } from '@/lib/hooks';
+import ProjectCreateForm from '@/components/ProjectCreateForm';
+import { ProjectType } from '@/types/project';
 
 export const ProjectManagement = () => {
-  return <div>프로젝트 관리 페이지</div>;
+  const userState = useRecoilValue(userAtom);
+  const [projectList, setProjectList] = useState<ProjectType[]>([]);
+  const { value, onChange } = useInput('');
+
+  const onSubmitNewProject = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!value.length) return;
+    const newProject = await createProject(value, userState.id as number);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const projects = await getAllOrgProjects(userState.organization!);
+      if (!projects) return;
+      console.log(projects);
+      setProjectList([...projects]);
+    })();
+  }, [userState.organization]);
+
+  return (
+    <>
+      <ProjectCreateForm onSubmitNewProject={onSubmitNewProject} onChange={onChange} />
+      {projectList.map((project, index) => {
+        <div key={index}>
+          <div>test</div>
+          <div>{project.name}</div>;
+        </div>;
+      })}
+    </>
+  );
 };
