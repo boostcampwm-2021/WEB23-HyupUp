3e86fd852d4798c95612c7d52827890a94d93bcc
ingong <insong1216@gmail.com>
2021-11-22 20:13:26 +0900

    layout: kanbanItem 이 dragEnter 상태일 때 파란색 줄 스타일링 [#87]

--- client/src/components/KanbanItem/style.ts
+++ client/src/components/KanbanItem/style.ts
@@ -1,15 +1,22 @@
 import styled from 'styled-components';
 import cancelicon from '@public/icons/cancel-icon.svg';
 
+interface KanbanItemPropType {
+  isDragEnter: boolean;
+}
+
 const Styled = {
-  KanBanItem: styled.article`
+  KanBanItem: styled.article<KanbanItemPropType>`
     width: 90%;
     height: 65px;
     border-radius: 8px;
     margin-top: 10px;
     display: flex;
-    cursor: move;
+    cursor: grab;
     background-color: ${({ theme }) => theme.color.gray100};
+    border-bottom: ${({ isDragEnter, theme }) => isDragEnter && `4px solid ${theme.color.blue200}`};
+    border-bottom-left-radius: ${({ isDragEnter }) => isDragEnter && '2px'};
+    border-bottom-right-radius: ${({ isDragEnter }) => isDragEnter && '2px'};
 
     input {
       background-color: ${({ theme }) => theme.color.gray100};
@@ -21,6 +28,7 @@ const Styled = {
 
       font: ${({ theme }) => theme.font.bold_small};
       font-size: 14px;
+      cursor: grab;
     }
   `,
 
