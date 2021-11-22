9fb7edaca0453428a911032a98a61e794a31ba97
rudy3091 <grayblack313@gmail.com>
2021-11-22 23:01:48 +0900

    refactor: 분리한 함수로 RoadmapBars 컴포넌트 리팩토링 [#26]
    RoadmapBars 컴포넌트에서 epicRenderInfo 를 만드는 로직을 분리함

--- client/src/components/RoadmapBars/index.tsx
+++ client/src/components/RoadmapBars/index.tsx
@@ -3,17 +3,9 @@ import React, { useState } from 'react';
 import S from './style';
 import RoadmapItem from '@/components/RoadmapItem';
 import { useEpicDispatch, useEpicState } from '@/lib/hooks/useContextHooks';
-import {
-  addDate,
-  getDateDiff,
-  isFormer,
-  isLatter,
-  isSameDay,
-  shouldRender,
-} from '@/lib/utils/date';
-import { toast } from 'react-toastify';
+import { addDate } from '@/lib/utils/date';
 import { updateEpicById } from '@/lib/api/epic';
-import { successMessage } from '@/lib/common/message';
+import { makeEpicRenderInfo } from '@/lib/utils/epic';
 
 const COLUMNS = 15;
 
@@ -24,66 +16,12 @@ interface RoadmapBarsProps {
   isToday: boolean;
 }
 
-// rangeFrom은 현재 캘린더뷰의 시작일자, rangeTo는 끝 일자
-// startAt은 에픽 항목의 시작일자, endAt은 에픽 항목의 끝 일자
-//
-// 에픽 막대를 렌더링해야하는 케이스 (<--> : 캘린더뷰, |--|: 에픽 항목)
-// 1: startAt이 rangeFrom 앞, endAt이 rangeFrom 뒤 (왼쪽에 걸쳐있는 경우)
-//    <------>
-// |----|
-//
-// 2: startAt이 rangeTo 앞, endAt이 rangeTo 뒤 (오른쪽에 걸쳐있는 경우)
-// <------>
-//      |-----|
-//
-// 3: startAt이 rangeFrom 뒤, endAt이 rangeTo 앞 (완전히 포함되는 경우)
-// <------>
-//   |--|
-//
-// 4: startAt이 rangeFrom 앞, endAt이 rangeTo 뒤 (완전히 포함하는 경우)
-//    <------>
-// |-----------|
-//
-// 그 외의 경우는 캘린더 뷰에 렌더링하지 않아야함
 const RoadmapBars = ({ rangeFrom, rangeTo, dayRow, isToday }: RoadmapBarsProps) => {
   const epics = useEpicState();
   const dispatchEpic = useEpicDispatch();
   const [nowDraggingId, setDraggingId] = useState(-1);
   const [isDraggingLeft, setDraggingLeft] = useState(false);
-  const epicRenderInfo = epics.map(({ id, startAt, endAt }) => {
-    const case1 = isFormer(startAt, rangeFrom) && isLatter(endAt, rangeFrom);
-    const case2 = isFormer(startAt, rangeTo) && isLatter(endAt, rangeTo);
-    const case3 = isLatter(startAt, rangeFrom) && isFormer(endAt, rangeTo);
-    const case4 = isFormer(startAt, rangeFrom) && isLatter(endAt, rangeTo);
-
-    const render = shouldRender({
-      start: startAt,
-      end: endAt,
-      from: rangeFrom,
-      to: rangeTo,
-    });
-    let startIndex = COLUMNS;
-    if (isSameDay(endAt, rangeFrom)) startIndex = 0;
-    else if (isSameDay(startAt, rangeTo)) startIndex = COLUMNS - 1;
-    else if (case1 || case4) startIndex = 0;
-    else if (render) startIndex = Math.min(getDateDiff(rangeFrom, startAt), COLUMNS);
-
-    let length = 0;
-    if (case1) length = getDateDiff(rangeFrom, endAt);
-    else if (case2) length = getDateDiff(startAt, rangeTo);
-    else if (case3) length = getDateDiff(startAt, endAt);
-    else if (case4) length = getDateDiff(rangeFrom, rangeTo);
-
-    const exceedsLeft = !isSameDay(startAt, rangeFrom) && isFormer(startAt, rangeFrom);
-    const exceedsRight = !isSameDay(endAt, rangeTo) && isLatter(endAt, rangeTo);
-    return {
-      index: startIndex,
-      id,
-      length,
-      exceedsLeft,
-      exceedsRight,
-    };
-  });
+  const epicRenderInfo = makeEpicRenderInfo(epics, { rangeFrom, rangeTo, columns: COLUMNS });
 
   return (
     <>
