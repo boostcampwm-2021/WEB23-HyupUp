4a1d50ccc4be1b134002b8fb4d13d5375ef757c0
Harry <haesoo9410@gmail.com>
2021-11-22 22:58:08 +0900

    feat: 프로젝트 배열 반환 api 생성 [#135]

--- server/src/Projects/Projects.controller.ts
+++ server/src/Projects/Projects.controller.ts
@@ -1,13 +1,40 @@
-import { Request, Response, NextFunction } from 'express';
-import { bodyValidator } from '../../lib/utils/requestValidator';
 import { getRepository } from 'typeorm';
+import { Request, Response } from 'express';
+import { bodyValidator } from '../../lib/utils/requestValidator';
 
 import Users from '../Users/Users.entity';
 import Projects from './Projects.entity';
+import { getUsers } from '../Users/Users.service';
 
-// TODO: User이용하여 조회
-export const getAllProjects = async (req: Request, res: Response, next: NextFunction) => {
-  return;
+const getProjects = (users: Users[]) => {
+  const result = [];
+  for (const user of users) {
+    result.push(...user.projects);
+  }
+  result.sort((a, b) => a.id - b.id);
+  // 중복 제거
+  if (result.length === 1) return result;
+  for (let i = 1; i < result.length; i++) {
+    if (result[i].id === result[i - 1].id) {
+      result[i - 1].id = 0;
+    }
+  }
+  return result.filter((project) => project.id !== 0);
+};
+
+export const getAllProjects = async (req: Request, res: Response) => {
+  try {
+    if (!req.params.id) {
+      throw new Error('params is not valid');
+    }
+    const users = await getUsers(+req.params.id);
+
+    const projects = getProjects(users);
+    res.json(projects);
+  } catch (error) {
+    const message = (error as Error).message;
+    res.status(401).json({ message });
+  }
 };
 
 export const createProject = async (req: Request, res: Response) => {
