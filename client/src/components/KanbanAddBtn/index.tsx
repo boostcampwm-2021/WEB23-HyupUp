29e51f42d2e2f85bd8f97fa691f1431c398f5407
ingong <insong1216@gmail.com>
2021-11-22 17:24:13 +0900

    refactor: StoryItem Order 를 순차적으로 가지도록 수정 [#87]

--- client/src/components/KanbanAddBtn/index.tsx
+++ client/src/components/KanbanAddBtn/index.tsx
@@ -1,49 +1,35 @@
-import React, { useState } from 'react';
-import { createStory, updateStoryWithId } from '@/lib/api/story';
+import React from 'react';
+import { postStory } from '@/lib/api/story';
 import { Button } from '@/lib/design';
 import { useStoryDispatch, useStoryState, useUserState } from '@/lib/hooks/useContextHooks';
 import { StoryType } from '@/types/story';
 
-const extractLastID = (array: Array<StoryType>): number => {
-  if (!array?.length) return 0;
-  return Math.max(...array.map((v) => v.id));
+//TODO projectID, epicId 주입 예정
+const initialItem = {
+  id: 0,
+  order: 0,
+  name: '',
+  status: 'TODO',
+  projectId: 1,
+  epicId: 1,
 };
 
 const KanbanAddBtn = () => {
   const userState = useUserState();
   const storyList = useStoryState();
   const dispatchStory = useStoryDispatch();
-  const lastStoryId = extractLastID(storyList);
-  const todoList = storyList.filter((item) => item.status === 'TODO');
-  //Todo 수정해야함
-  const initialItem = {
-    id: lastStoryId + 1,
-    name: '',
-    status: 'TODO',
-    order: 1,
-    projectId: 1,
-    epicId: 1,
-  };
+  const listLargestOrder = storyList
+    .filter((item) => item.status === 'TODO')
+    .map((v) => Number(v.order))
+    .find((v) => v === Math.max(v));
 
   const addStory = async () => {
-    const lastTodo = todoList?.length > 0 ? todoList[todoList.length - 1] : initialItem;
-    const beforeLastTodo = todoList?.length > 1 ? todoList[todoList.length - 2] : initialItem;
-    switch (todoList.length) {
-      case 0:
-        break;
-      case 1:
-        dispatchStory({ type: 'UPDATE_STORY', story: { ...lastTodo, order: 0 } });
-        updateStoryWithId({ ...lastTodo, order: 0 });
-        break;
-      default:
-        dispatchStory({
-          type: 'UPDATE_STORY',
-          story: { ...lastTodo, order: (1 + Number(beforeLastTodo.order)) / 2 },
-        });
-        updateStoryWithId({ ...lastTodo, order: (1 + Number(beforeLastTodo.order)) / 2 });
-    }
-    dispatchStory({ type: 'ADD_STORY', story: initialItem });
-    createStory({ ...initialItem, projectId: userState.currentProjectId });
+    const newStoryItem = await postStory({
+      ...initialItem,
+      order: storyList.length > 0 ? listLargestOrder : 0,
+      projectId: userState.currentProjectId,
+    });
+    dispatchStory({ type: 'ADD_STORY', story: newStoryItem as StoryType });
   };
 
   return (
