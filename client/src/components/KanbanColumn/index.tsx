5d2cd239e9d811b6c93a461d6b1bd31798d51045
ingong <insong1216@gmail.com>
2021-11-23 01:06:57 +0900

    feat: 동일 칼럼에서 최상단, 최하단으로 Item 이동 기능 구현 완료 [#87]
    - 동일 컬럼 내에서 아이템 이동 기능 구현 완료

--- client/src/components/KanbanColumn/index.tsx
+++ client/src/components/KanbanColumn/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Styled from '@/components/KanbanColumn/style';
 import { KanbanItem, KanbanAddBtn } from '@/components';
-import { StatusType, KanbanType, StoryType } from '@/types/story';
+import { StatusType, KanbanType } from '@/types/story';
 import { useStoryDispatch } from '@/lib/hooks/useContextHooks';
 
 const KanbanColumn = ({
@@ -28,38 +28,68 @@ const KanbanColumn = ({
     category: StatusType,
   ) => {
     dragOverRef.current = order;
-    const dragOverItem = storyList.find((v) => v.id === dragOverRef.current);
-    // TODO immerJS 를 통해서 새로운 배열을 만들고, 이에 대해서 update 하는 함수
-    // draggingRef.current = dragOverRef.current;
   };
 
-  console.log(storyList);
   const handleDragDrop = (
     e: React.DragEvent<HTMLElement>,
     order?: number,
-    category: StatusType,
+    category?: StatusType,
   ) => {
-    const toBeChangeItem = storyList.find((v) => v.order === draggingRef.current);
-    const dragOverOrderList = storyList
-      .map((v) => Number(v.order))
-      .filter((v) => v >= Number(dragOverRef.current))
-      .slice(0, 2);
-    const averageOrder = dragOverOrderList.reduce((prev, cur) => prev + cur, 0) / 2;
-    if (!toBeChangeItem) return;
-    dispatchStory({ type: 'UPDATE_STORY', story: { ...toBeChangeItem, order: averageOrder } });
-    draggingRef.current = null;
-    dragOverRef.current = null;
+    // 동일한 칼럼 내의 제일 상단에 Item 을 놓을 때
+    if (isTopEnter) {
+      const toBeChangeItem = storyList.find((v) => v.order === draggingRef.current);
+      if (!toBeChangeItem) return;
+      const firstnSecondItem = storyList
+        .sort((a, b) => Number(a.order) - Number(b.order))
+        .slice(0, 2);
+      const averageOrder = firstnSecondItem.length > 1 ? Number(firstnSecondItem[1].order) / 2 : 1;
+      dispatchStory({
+        type: 'UPDATE_STORY',
+        story: { ...toBeChangeItem, order: 0 },
+      });
+      dispatchStory({
+        type: 'UPDATE_STORY',
+        story: { ...firstnSecondItem[0], order: averageOrder },
+      });
+      setTopEnter((isTopEnter) => !isTopEnter);
+      draggingRef.current = null;
+      dragOverRef.current = null;
+      return;
+    }
+
+    // 동일한 컬럼 내의 Item 의 사이에 위치
+    if (!isTopEnter) {
+      const toBeChangeItem = storyList.find((v) => v.order === draggingRef.current);
+      const dragOverOrderList = storyList
+        .map((v) => Number(v.order))
+        .filter((v) => v >= Number(dragOverRef.current))
+        .slice(0, 2);
+      const orderSum = dragOverOrderList.reduce((prev, cur) => prev + cur, 0);
+      const avgOrderSum = dragOverOrderList.length > 1 ? orderSum / 2 : orderSum + 1;
+
+      if (!toBeChangeItem) return;
+      dispatchStory({ type: 'UPDATE_STORY', story: { ...toBeChangeItem, order: avgOrderSum } });
+      draggingRef.current = null;
+      dragOverRef.current = null;
+    }
+
+    // 동일 칼럼의 맨 아래 부분에 위치할 때
+    // 다른 칼럼 내에 Item 이 위치
   };
 
   return (
     <Styled.Column
-      onDragEnter={() => setTopEnter((isTopEnter) => !isTopEnter)}
-      onDragLeave={() => setTopEnter(false)}
-      onDrop={() => handleDragDrop}
+    // onDragEnter={() => setTopEnter((isTopEnter) => !isTopEnter)}
+    // onDragLeave={() => setTopEnter(false)}
+    // onDrop={() => handleDragDrop}
     >
       <Styled.KanBanColumnTitle
+        onDragOver={(e) => e.preventDefault()}
         onDragEnter={() => setTopEnter((isTopEnter) => !isTopEnter)}
         onDragLeave={() => setTopEnter(false)}
+        onDrop={(e) => {
+          handleDragDrop(e, 1, 'TODO');
+        }}
         isTopEnter={isTopEnter}
       >
         {category}
