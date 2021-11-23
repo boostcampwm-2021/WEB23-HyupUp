5b5c76fac181b24b930d1c40327710cbacce1529
이찬호 <chanholee275@gmail.com>
2021-11-23 16:21:32 +0900

    refactor: 가독성을 위하여 if...else문을 switch문으로 변경

--- client/src/layers/GroupManagement/index.tsx
+++ client/src/layers/GroupManagement/index.tsx
@@ -1,90 +1,118 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './style';
 import { Modal } from '@/lib/design';
 
-import { deleteUserById, modifyUserAdminById } from '@/lib/api/user';
+import {
+  deleteUserById,
+  getUsersByOrganization,
+  modifyUserAdminById,
+  UserProfile,
+} from '@/lib/api/user';
 import TeamInviteBar from '@/components/TeamInviteBar';
 import { TeamItemViewer } from '@/components/TeamItemViewer';
+import userAtom from '@/recoil/user';
+import { useRecoilValue } from 'recoil';
 
 const ADMIN = true;
 const TEAMMATE = false;
 
 const generateMessage = (message: string) => {
-  return message === '팀원에서 제외' ? (
-    <S.ModalText>정말로 팀원에서 제외하겠습니까?</S.ModalText>
-  ) : message === '팀원으로 변경' ? (
-    <S.ModalText>정말 팀원으로 변경하시겠습니까?</S.ModalText>
-  ) : (
-    <S.ModalText>정말 관리자로 변경하시겠습니까?</S.ModalText>
-  );
+  switch (message) {
+    case '팀원에서 제외':
+      return <S.ModalText>정말로 팀원에서 제외하겠습니까?</S.ModalText>;
+    case '팀원으로 변경':
+      return <S.ModalText>정말 팀원으로 변경하시겠습니까?</S.ModalText>;
+    default:
+      return <S.ModalText>정말 관리자로 변경하시겠습니까?</S.ModalText>;
+  }
 };
 
 // TODO: 함수 분리하기
 export const GroupManagement = () => {
+  const userState = useRecoilValue(userAtom);
   const [showModal, setShowModal] = useState(false);
-  const [forceRender, setForceRender] = useState({ flag: false });
-  const [adminState, setAdminState] = useState<{ message: string; index: number }>({
+  const [userList, setUserList] = useState<Array<UserProfile>>([]);
+  const [adminState, setAdminState] = useState<{ message: string; id: number }>({
     message: '',
-    index: -1,
+    id: 0,
   });
+
+  useEffect(() => {
+    (async () => {
+      const newUserList = await getUsersByOrganization(userState.organization as number);
+      setUserList(newUserList.filter((el) => el.index !== userState.id));
+    })();
+  }, [userState.id, userState.organization]);
 
   const onModalButtonClick = () => {
     const newShowModal = !showModal;
     setShowModal(newShowModal);
   };
 
-  const showEditModal = (e: React.MouseEvent, index: number) => {
+  const showEditModal = (e: React.MouseEvent, id: number) => {
     const text = (e.target as HTMLLIElement).innerText;
-
-    if (text === '팀원에서 제외') {
-      setAdminState({ message: '팀원에서 제외', index: index });
-    } else if (text === '팀원으로 변경') {
-      setAdminState({ message: '팀원으로 변경', index: index });
-    } else if (text === '관리자로 변경') {
-      setAdminState({ message: '관리자로 변경', index: index });
+    switch (text) {
+      case '팀원에서 제외':
+        setAdminState({ message: '팀원에서 제외', id: id });
+        break;
+      case '팀원으로 변경':
+        setAdminState({ message: '팀원으로 변경', id: id });
+        break;
+      default:
+        setAdminState({ message: '관리자로 변경', id: id });
+        break;
     }
   };
 
   const modifyAdminState = async () => {
-    if (adminState.index === -1) return;
-    if (adminState.message === '팀원에서 제외') {
-      await deleteUserById(adminState.index);
-      setAdminState({ message: '', index: -1 });
-    } else if (adminState.message === '팀원으로 변경') {
-      await modifyUserAdminById(adminState.index, TEAMMATE);
-      setAdminState({ message: '', index: -1 });
-    } else if (adminState.message === '관리자로 변경') {
-      await modifyUserAdminById(adminState.index, ADMIN);
-      setAdminState({ message: '', index: -1 });
+    if (!adminState.id) return;
+    switch (adminState.message) {
+      case '팀원에서 제외':
+        setUserList(userList.filter((el) => el.index === adminState.id));
+        await deleteUserById(adminState.id);
+        setAdminState({ message: '', id: 0 });
+        break;
+      case '팀원으로 변경':
+        setUserList(
+          userList.map((el) => (el.index === adminState.id ? { ...el, admin: false } : el)),
+        );
+        await modifyUserAdminById(adminState.id, TEAMMATE);
+        setAdminState({ message: '', id: 0 });
+        break;
+      default:
+        setUserList(
+          userList.map((el) => (el.index === adminState.id ? { ...el, admin: true } : el)),
+        );
+        await modifyUserAdminById(adminState.id, ADMIN);
+        setAdminState({ message: '', id: 0 });
+        break;
     }
-    // TeamItemViewer 강제 렌더링
-    setForceRender({ ...forceRender });
   };
 
   return (
     <S.Container>
-      {showModal && (
+      {
         <Modal
           shouldConfirm={false}
           title="초대할 사람의 이메일을 입력해주세요."
-          visible={true}
+          visible={showModal}
           onClose={onModalButtonClick}
         >
           <TeamInviteBar onCloseClick={onModalButtonClick} />
         </Modal>
-      )}
+      }
       <S.ItemListViewer>
         <TeamItemViewer
           onModalButtonClick={onModalButtonClick}
           showEditModal={showEditModal}
-          forceRender={forceRender}
+          userProfileList={userList}
         />
       </S.ItemListViewer>
       {adminState.message !== '' && (
         <Modal
           shouldConfirm={true}
           visible={true}
-          onClose={() => setAdminState({ message: '', index: -1 })}
+          onClose={() => setAdminState({ message: '', id: 0 })}
           onClickOk={modifyAdminState}
         >
           {generateMessage(adminState.message)}
