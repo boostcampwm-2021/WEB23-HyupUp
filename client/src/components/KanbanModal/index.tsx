14dea919f7e73252cbb86b85cffd0e36b0179e63
ingong <insong1216@gmail.com>
2021-11-24 00:25:12 +0900

    feat: 아이템 클릭 시 스토리 이름이 나타난다

--- client/src/components/KanbanModal/index.tsx
+++ client/src/components/KanbanModal/index.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Modal } from '@/lib/design';
+import { StoryType } from '@/types/story';
 
 interface KanbnaModalType {
   isItemClick: boolean;
+  story: StoryType;
 }
 
-const KanbanModal = ({ isItemClick }: KanbnaModalType) => {
+const KanbanModal = ({ isItemClick, story }: KanbnaModalType) => {
   const [isOpen, setIsOpen] = useState(false);
   const handleCloseClick = () => {
     setIsOpen((prev) => !prev);
@@ -16,7 +18,9 @@ const KanbanModal = ({ isItemClick }: KanbnaModalType) => {
   }, [isItemClick]);
 
   return (
-    <Modal shouldConfirm={false} visible={isOpen} onClose={handleCloseClick} size="LARGE"></Modal>
+    <Modal shouldConfirm={false} visible={isOpen} onClose={handleCloseClick} size="LARGE">
+      {story.name}
+    </Modal>
   );
 };
 
