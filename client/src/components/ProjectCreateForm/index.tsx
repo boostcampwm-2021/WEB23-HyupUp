3f781184451305107a1f66b1f79d95357f73fc8e
Harry <haesoo9410@gmail.com>
2021-11-23 21:30:48 +0900

    refactor: submit후 값이 초기화되도록 새로운 useInput 적용

--- client/src/components/ProjectCreateForm/index.tsx
+++ client/src/components/ProjectCreateForm/index.tsx
@@ -5,12 +5,13 @@ import { Button } from '@/lib/design';
 interface Props {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onSubmitNewProject: (e: React.FormEvent) => void;
+  value: string;
 }
 
-const ProjectCreateForm = ({ onSubmitNewProject, onChange }: Props) => {
+const ProjectCreateForm = ({ onSubmitNewProject, onChange, value }: Props) => {
   return (
     <Styled.Form onSubmit={onSubmitNewProject}>
-      <Styled.Input placeholder="프로젝트 이름을 입력하세요" onChange={onChange} />
+      <Styled.Input placeholder="프로젝트 이름을 입력하세요" onChange={onChange} value={value} />
       <Button
         size="large"
         category="default"
