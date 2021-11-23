5ba8e6fad2f6f84ceff83f5cce02f9a27d35aab9
ingong <insong1216@gmail.com>
2021-11-23 21:07:45 +0900

    fix: StoryType 의 모든 항목을 optional 로 부여함[#87]
    - 왜 이렇게 바꿔야하는지 모르겠음. util 로 분리하는 과정에서 일치하지 않는 다는 오류 발생

--- client/src/types/story.d.ts
+++ client/src/types/story.d.ts
@@ -1,8 +1,8 @@
 export type StatusType = 'TODO' | 'IN_PROGRESS' | 'DONE';
 
 export type StoryType = {
-  name: string;
-  status: StoryStatusType;
+  name?: string;
+  status?: StoryStatusType;
   id?: number;
   order?: number;
   projectId?: number;
