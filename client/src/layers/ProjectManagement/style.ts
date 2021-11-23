5ea4f8c9f2f580e12135049c1b000a97e9acc50a
Harry <haesoo9410@gmail.com>
2021-11-23 17:41:09 +0900

    layout: 프로젝트 관리페이지 레이아웃

---
+++ client/src/layers/ProjectManagement/style.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const Styled = {
+  ProjectManagementWrapper: styled.section`
+    margin-left: 20px;
+  `,
+  ProjectList: styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+
+    width: 950px;
+    height: 500px;
+    margin-top: 20px;
+
+    background-color: ${({ theme }) => theme.color.gray100};
+    border-radius: 8px;
+
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  `,
+};
+
+export default Styled;
