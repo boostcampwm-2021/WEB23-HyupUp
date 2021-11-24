ac0c1de9ce3972c212a094a59e9ce25213f4487b
ingong <insong1216@gmail.com>
2021-11-24 21:09:30 +0900

    layout: 칸반 모달 스타일 수정 [#91]

--- client/src/components/KanbanModal/KanbanTask/index.tsx
+++ client/src/components/KanbanModal/KanbanTask/index.tsx
@@ -3,7 +3,16 @@ import { KanbanTaskWrapper } from './style';
 import { BackLogTaskProps } from '@/types/task';
 
 const KanbanTask = ({ task }: { task: BackLogTaskProps }) => {
-  return <KanbanTaskWrapper>{task.name}</KanbanTaskWrapper>;
+  console.log(task);
+  return (
+    <KanbanTaskWrapper>
+      <h4>{task.name}</h4>
+      <p>
+        <img src={task.userImage} alt="userimage" />
+        <span>{task.user}</span>
+      </p>
+    </KanbanTaskWrapper>
+  );
 };
 
 export default KanbanTask;
--- client/src/components/KanbanModal/KanbanTask/style.ts
+++ client/src/components/KanbanModal/KanbanTask/style.ts
@@ -1,6 +1,36 @@
 import styled from 'styled-components';
 
 export const KanbanTaskWrapper = styled.article`
-  width: 800px;
-  height: 40px;
+  width: 700px;
+  height: 70px;
+  font: ${({ theme }) => theme.font.bold_regular};
+  background-color: ${({ theme }) => theme.color.gray100};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+  padding: 10px;
+
+  h4 {
+    padding-left: 30px;
+  }
+
+  p {
+    width: 130px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-right: 30px;
+  }
+
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+
+  span {
+    font: ${({ theme }) => theme.font.bold_small};
+    font-size: 14px;
+  }
 `;
