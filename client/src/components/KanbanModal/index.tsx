fc2e051fafffa5dea6039a477c2b16284485fb33
ingong <insong1216@gmail.com>
2021-11-24 17:55:37 +0900

    feat: 칸반 클릭 시 해당 Task 를 받아오도록 함수 호출

--- client/src/components/KanbanModal/index.tsx
+++ client/src/components/KanbanModal/index.tsx
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Modal } from '@/lib/design';
 import { StoryType } from '@/types/story';
-
+import { getTasksByStoryId } from '@/lib/api/task';
+import { BackLogTaskProps } from '@/types/task';
 interface KanbnaModalType {
   isItemClick: boolean;
   story: StoryType;
 }
 
+type ResultType = undefined | Array<BackLogTaskProps>;
+
 const KanbanModal = ({ isItemClick, story }: KanbnaModalType) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [tasks, setTasks] = useState<Array<BackLogTaskProps>>([]);
+
   const handleCloseClick = () => {
     setIsOpen((prev) => !prev);
   };
@@ -16,6 +21,14 @@ const KanbanModal = ({ isItemClick, story }: KanbnaModalType) => {
   useEffect(() => {
     setIsOpen(isItemClick);
   }, [isItemClick]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      if (!isItemClick) return;
+      const result: ResultType = await getTasksByStoryId(story.id as number);
+    };
+    fetchTasks();
+  }, [story.id, isItemClick]);
 
   return (
     <Modal shouldConfirm={false} visible={isOpen} onClose={handleCloseClick} size="LARGE">
