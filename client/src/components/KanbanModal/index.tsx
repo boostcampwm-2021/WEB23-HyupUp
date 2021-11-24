fd2b05a0f43e121d34e5a2cb9709f36dd7415cba
ingong <insong1216@gmail.com>
2021-11-25 01:09:36 +0900

    fix: Modal 열고 닫는 이슈 해결 [#91]

--- client/src/components/KanbanModal/index.tsx
+++ client/src/components/KanbanModal/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Modal } from '@/lib/design';
+import { Modal, Button } from '@/lib/design';
 import ContentWrapper from '@/components/KanbanModal/style';
 import { BackLogTaskProps } from '@/types/task';
 import { StoryType } from '@/types/story';
@@ -8,39 +8,38 @@ import { useRecoilValue } from 'recoil';
 import userAtom from '@/recoil/user';
 import KanbanTask from './KanbanTask/index';
 
-interface KanbnaModalType {
-  isItemClick: boolean;
+interface KanbanModalType {
   story: StoryType;
+  isItemModalOpen: boolean;
+  setModalOpen: (arg: boolean) => void;
 }
 
 type ResultType = undefined | Array<BackLogTaskProps>;
 
-const KanbanModal = ({ isItemClick, story }: KanbnaModalType) => {
+const KanbanModal = ({ story, isItemModalOpen, setModalOpen }: KanbanModalType) => {
   const user = useRecoilValue(userAtom);
-  const [isOpen, setIsOpen] = useState(false);
   const [tasks, setTasks] = useState<ResultType>([]);
 
   const handleCloseClick = () => {
-    setIsOpen((prev) => !prev);
+    setModalOpen(!isItemModalOpen);
   };
 
   useEffect(() => {
-    setIsOpen(isItemClick);
-  }, [isItemClick]);
-
-  useEffect(() => {
     const fetchTasks = async () => {
-      if (!isItemClick) return;
+      if (!isItemModalOpen) return;
       const result: ResultType = await getTasksByStoryId(story.id as number);
       setTasks(result);
     };
     fetchTasks();
-  }, [story.id, isItemClick]);
+  }, [story.id, isItemModalOpen]);
 
   return (
-    <Modal shouldConfirm={false} visible={isOpen} onClose={handleCloseClick} size="LARGE">
+    <Modal shouldConfirm={false} visible={isItemModalOpen} onClose={handleCloseClick} size="LARGE">
       <ContentWrapper>
         <h3>{story.name}</h3>
+        <Button category={'default'} size={'small'}>
+          ADD TASK
+        </Button>
         {tasks?.map((task) => (
           <KanbanTask key={task.id} task={task} />
         ))}
