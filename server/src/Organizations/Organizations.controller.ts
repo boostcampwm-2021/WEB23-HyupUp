2226d564dcaf91113040dc4f86cb32df513b3fcd
이찬호 <chanholee275@gmail.com>
2021-11-24 20:53:58 +0900

    feat: 조직의 존재 여부를 확인하기 위한 API 작성 [#141]
    조직을 새로 생성할 것인지, 기존 조직에 참여할 것인지를 보여주기 위해 API 작성

---
+++ server/src/Organizations/Organizations.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { queryValidator } from '../../lib/utils/requestValidator';
+import { getRepository } from 'typeorm';
+import Organizations from './Organizations.entity';
+
+export const getOrganizationByName = async (req: Request, res: Response) => {
+  try {
+    if (!queryValidator(req.query, ['name'])) throw new Error('query is not valid');
+    const organizationRepository = getRepository(Organizations);
+    const organization = await organizationRepository.findOne({ where: { room: req.query.name } });
+    organization ? res.status(200).end() : res.status(204).end();
+  } catch (e) {
+    const err = e as Error;
+    res.status(400).json({ message: err.message });
+  }
+};
