b3a1b4c9f35c15750b0f65c4a0c251b68460349d
이찬호 <chanholee275@gmail.com>
2021-11-24 20:49:08 +0900

    feat: 아바타를 객체로 만들어서 bundling하게 만듦 [#141]
    서버에서 직접 가져가는 것이 아닌, bundle된 폴더에서 가져가도록 만듦

---
+++ client/src/lib/common/avatar/index.ts
@@ -0,0 +1,107 @@
+import Boy0 from '@public/image/Boy-0.svg';
+import Boy1 from '@public/image/Boy-1.svg';
+import Boy2 from '@public/image/Boy-2.svg';
+import Boy3 from '@public/image/Boy-3.svg';
+import Boy4 from '@public/image/Boy-4.svg';
+import Boy5 from '@public/image/Boy-5.svg';
+import Boy6 from '@public/image/Boy-6.svg';
+import Boy7 from '@public/image/Boy-7.svg';
+import Boy8 from '@public/image/Boy-8.svg';
+import Boy9 from '@public/image/Boy-9.svg';
+import Boy10 from '@public/image/Boy-10.svg';
+import Boy11 from '@public/image/Boy-11.svg';
+import Boy12 from '@public/image/Boy-12.svg';
+import Boy13 from '@public/image/Boy-13.svg';
+import Boy14 from '@public/image/Boy-14.svg';
+import Boy15 from '@public/image/Boy-15.svg';
+import Boy16 from '@public/image/Boy-16.svg';
+import Girl0 from '@public/image/Girl-0.svg';
+import Girl1 from '@public/image/Girl-1.svg';
+import Girl2 from '@public/image/Girl-2.svg';
+import Girl3 from '@public/image/Girl-3.svg';
+import Girl4 from '@public/image/Girl-4.svg';
+import Girl5 from '@public/image/Girl-5.svg';
+import Girl6 from '@public/image/Girl-6.svg';
+import Girl7 from '@public/image/Girl-7.svg';
+import Girl8 from '@public/image/Girl-8.svg';
+import Girl9 from '@public/image/Girl-9.svg';
+import Girl10 from '@public/image/Girl-10.svg';
+import Girl11 from '@public/image/Girl-11.svg';
+import Girl12 from '@public/image/Girl-12.svg';
+import Girl13 from '@public/image/Girl-13.svg';
+import Girl14 from '@public/image/Girl-14.svg';
+import Girl15 from '@public/image/Girl-15.svg';
+import Girl16 from '@public/image/Girl-16.svg';
+
+export type ImageType =
+  | 'Boy0'
+  | 'Boy1'
+  | 'Boy2'
+  | 'Boy3'
+  | 'Boy4'
+  | 'Boy5'
+  | 'Boy6'
+  | 'Boy7'
+  | 'Boy8'
+  | 'Boy9'
+  | 'Boy10'
+  | 'Boy11'
+  | 'Boy12'
+  | 'Boy13'
+  | 'Boy14'
+  | 'Boy15'
+  | 'Boy16'
+  | 'Girl0'
+  | 'Girl1'
+  | 'Girl2'
+  | 'Girl3'
+  | 'Girl4'
+  | 'Girl5'
+  | 'Girl6'
+  | 'Girl7'
+  | 'Girl8'
+  | 'Girl9'
+  | 'Girl10'
+  | 'Girl11'
+  | 'Girl12'
+  | 'Girl13'
+  | 'Girl14'
+  | 'Girl15'
+  | 'Girl16';
+
+export default {
+  Boy0,
+  Boy1,
+  Boy2,
+  Boy3,
+  Boy4,
+  Boy5,
+  Boy6,
+  Boy7,
+  Boy8,
+  Boy9,
+  Boy10,
+  Boy11,
+  Boy12,
+  Boy13,
+  Boy14,
+  Boy15,
+  Boy16,
+  Girl0,
+  Girl1,
+  Girl2,
+  Girl3,
+  Girl4,
+  Girl5,
+  Girl6,
+  Girl7,
+  Girl8,
+  Girl9,
+  Girl10,
+  Girl11,
+  Girl12,
+  Girl13,
+  Girl14,
+  Girl15,
+  Girl16,
+};
