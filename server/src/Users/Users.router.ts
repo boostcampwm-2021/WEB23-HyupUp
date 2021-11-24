51ee694c15ee52731ac8e1654041b11eb7f16d23
이찬호 <chanholee275@gmail.com>
2021-11-24 15:24:11 +0900

    fix: 사진 업로드 기능 제거에 따른 관련 코드 삭제

--- server/src/Users/Users.router.ts
+++ server/src/Users/Users.router.ts
@@ -7,23 +7,10 @@ import {
   signUpUser,
   updateUserAdminById,
 } from './Users.controller';
-import * as multer from 'multer';
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'upload');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer.default({ storage: storage }).single('file');
 
 const router = express.Router();
 router.get('/', handleGet);
 router.get('/organization', getUsersByOrganization);
-router.get('/image', upload);
 
 router.put('/admin/:id', updateUserAdminById);
 router.delete('/:id', deleteUserById);
