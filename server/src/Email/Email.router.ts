27be7c048023d52a5f9eda93480979e528bfc702
이찬호 <chanholee275@gmail.com>
2021-11-25 19:58:46 +0900

    fix: 외부에서 요청 보낼 때, session을 확인하지 않도록 수정

--- server/src/Email/Email.router.ts
+++ server/src/Email/Email.router.ts
@@ -5,6 +5,6 @@ import { inviteByEmail, isValidEmail } from './Email.controller';
 const router = express.Router();
 
 router.post('/', [authValidator, inviteByEmail]);
-router.get('/verify/:token', [authValidator, isValidEmail]);
+router.get('/verify/:token', isValidEmail);
 
 export default router;
