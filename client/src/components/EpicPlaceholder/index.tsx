afb5331490171a0f52e1c910f084cc7004a41b88
rudy3091 <grayblack313@gmail.com>
2021-11-25 17:32:44 +0900

    feat: EpicPlaceholder에 모달을 사용하도록 변경 [#15]
    visible props 를 받아와 EpicPlacholder에 사용
    스타일작업은 아직 하지 않음

--- client/src/components/EpicPlaceholder/index.tsx
+++ client/src/components/EpicPlaceholder/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import S from './style';
+import Modal from '@/lib/design/Modal';
 
 interface EpicPlaceholderProps {
   visible: boolean;
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
   handleSubmit(epicName: string): void;
 }
 
-const EpicPlaceholder = ({ visible, handleSubmit }: EpicPlaceholderProps) => {
+const EpicPlaceholder = ({ visible, setVisible, handleSubmit }: EpicPlaceholderProps) => {
   const [value, setValue] = React.useState<string>('');
 
   const handleFormSubmit = (ev: React.FormEvent) => {
@@ -21,9 +23,17 @@ const EpicPlaceholder = ({ visible, handleSubmit }: EpicPlaceholderProps) => {
 
   return (
     <S.Container>
-      <form onSubmit={handleFormSubmit}>
-        {visible ? <input type="text" value={value} onChange={handleChange} /> : undefined}
-      </form>
+      <Modal
+        shouldConfirm={false}
+        visible={visible}
+        title="새로운 에픽 생성"
+        size="LARGE"
+        onClose={() => setVisible(false)}
+      >
+        <form onSubmit={handleFormSubmit}>
+          <input type="text" value={value} onChange={handleChange} />
+        </form>
+      </Modal>
     </S.Container>
   );
 };
