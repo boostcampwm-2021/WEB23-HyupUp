715d429673e9bc14fc1b334fc1d2cc060fa5d3f1
Harry <haesoo9410@gmail.com>
2021-11-25 23:41:00 +0900

    chore: 토스트 경고 메시지 추가

--- client/src/lib/common/message/index.ts
+++ client/src/lib/common/message/index.ts
@@ -1,2 +1,3 @@
 export { default as errorMessage } from './error';
 export { default as successMessage } from './success';
+export { default as warningMessage } from './warning';
---
+++ client/src/lib/common/message/warning.ts
@@ -0,0 +1,5 @@
+export const ADMIN_ACCESS = '관리자 권한이 필요합니다.';
+
+export default {
+  ADMIN_ACCESS,
+};
