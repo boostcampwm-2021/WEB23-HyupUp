417e75453ae7faee2ca27345e8a17f69aaf22336
이찬호 <chanholee275@gmail.com>
2021-11-25 20:46:07 +0900

    feat: TEST 로그인 버튼 위치 변경

--- client/src/pages/LogInPage/index.tsx
+++ client/src/pages/LogInPage/index.tsx
@@ -3,12 +3,29 @@ import Button from '@/lib/design/Button';
 import { Styled } from './style';
 import { LogInForm } from '@/components/LogInForm';
 import { useHistory } from 'react-router-dom';
+import { sudoLogIn } from '@/lib/api/user';
+import { UserState } from '@/contexts/userContext';
+import { taskSortByUpdate } from '@/lib/utils/sort';
+import userAtom from '@/recoil/user';
+import { useSetRecoilState } from 'recoil';
 
 const LogInPage = () => {
   const history = useHistory();
+  const setUserState = useSetRecoilState(userAtom);
+
   const onClickSignIn = () => {
     history.push('/signup');
   };
+
+  const onClickSudoSignIn = async () => {
+    const userData = (await sudoLogIn()) as UserState;
+    if (userData.id) {
+      userData.privateTasks!.sort((a, b) => taskSortByUpdate(a, b));
+      userData.projectTasks!.sort((a, b) => taskSortByUpdate(a, b));
+    }
+    setUserState(userData);
+  };
+
   return (
     <Styled.Container>
       <Styled.LogoContainer>
@@ -19,6 +36,9 @@ const LogInPage = () => {
         <Button category="default" size="large" type="button" onClick={onClickSignIn}>
           HyupUp 회원가입
         </Button>
+        <Styled.SudoLogin type="button" onClick={onClickSudoSignIn}>
+          TEST 계정으로 시작하기
+        </Styled.SudoLogin>
       </Styled.ContentContainer>
     </Styled.Container>
   );
