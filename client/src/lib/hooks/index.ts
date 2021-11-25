f145b9228ed606bfea0018f0ee0040febdb25991
rudy3091 <grayblack313@gmail.com>
2021-11-25 16:02:59 +0900

    feat: useStoriesAboutEpic 훅 추가 [#29]
    에픽 아이디를 주면 연동된 스토리를 상태별로 분류해서 반환하는 함수를
    반환함

--- client/src/lib/hooks/index.ts
+++ client/src/lib/hooks/index.ts
@@ -3,3 +3,4 @@ export { default as useOutSideClick } from './useOutSideClick';
 export { default as useSocketSend } from './useSocketSend';
 export { default as useSocketReceive } from './useSocketReceive';
 export { default as useTabs } from './useTabs';
+export { default as useStoriesAboutEpic } from './useStoriesAboutEpic';
