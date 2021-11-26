f2c1a3ba51141c69873250bcf008c5a2eb1e3b26
rudy3091 <grayblack313@gmail.com>
2021-11-27 01:22:25 +0900

    feat: 스피너 컴포넌트 추가 [#192]
    CircleSpinner와 다른 디자인을 가진 스피너 컴포넌트 BarsSpinner 추가

---
+++ client/src/lib/design/Spinner/Bars/style.ts
@@ -0,0 +1,57 @@
+import styled, { keyframes } from 'styled-components';
+
+const anim = keyframes`
+  0% {
+    height: 100%;
+  }
+  20% {
+    height: 10%;
+  }
+  80% {
+    height: 10%;
+  }
+  100% {
+    height: 100%;
+  }
+`;
+
+const S = {
+  Container: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    // FIXME: 수치를 어떻게 정해야하나?
+    width: 100%;
+    height: 100%;
+  `,
+  BarsWrapper: styled.div`
+    width: 70px;
+    height: 70px;
+
+    display: flex;
+    align-items: center;
+  `,
+  Line: styled.line<{ count: number; duration: number }>`
+    width: 5px;
+
+    background-color: ${({ theme }) => theme.color.blue200};
+    animation: ${anim} ${(props) => props.duration}s linear infinite;
+
+    & + & {
+      margin-left: 5px;
+    }
+
+    ${({ count, duration }) =>
+      [...Array(count)]
+        .map(
+          (_, i) =>
+            `&:nth-child(${i + 1}) {
+      animation-delay: ${(duration / (count * 2)) * (i + 1)}s;
+    }`,
+        )
+        .join('\n\n')}
+  `,
+};
+
+export default S;
