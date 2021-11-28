fd572392497ff0a95386e94ace911e7034709521
Harry <haesoo9410@gmail.com>
2021-11-28 16:41:55 +0900

    layout: 스피너 디자인 컴포넌트 제작 [#192]

--- client/src/lib/design/index.ts
+++ client/src/lib/design/index.ts
@@ -1,3 +1,5 @@
 export { default as Button } from './Button/index';
 export { default as DropDown } from './DropDown/index';
 export { default as Modal } from './Modal/index';
+export { default as SearchBar } from './SearchBar';
+export { default as Spinner } from './Spinner';
