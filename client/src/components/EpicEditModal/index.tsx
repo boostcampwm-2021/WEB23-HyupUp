1f243be73145670720027b22b89aa19f5dd79685
rudy3091 <grayblack313@gmail.com>
2021-11-29 01:01:15 +0900

    refactor: 에픽 수정 모달 수정
    시작일과 종료일 라벨 추가
    시작일/종료일을 입력받을 수 있는 input type date 입력 추가

--- client/src/components/EpicEditModal/index.tsx
+++ client/src/components/EpicEditModal/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import S from './style';
-import { Button } from '@/lib/design';
 import { EpicType } from '@/types/epic';
+import { getYMD } from '@/lib/utils/date';
 
 interface EpicEditModalProps {
   epicData: EpicType;
@@ -10,10 +10,40 @@ interface EpicEditModalProps {
   handleFormSubmit: (e: React.FormEvent) => void;
 }
 
+const formatDate = (date: Date) => {
+  const { year, month, day } = getYMD(date);
+  return `${year}-${month + 1}-${day}`;
+};
+
 const EpicEditModal = ({ epicData, value, handleChange, handleFormSubmit }: EpicEditModalProps) => {
+  const [startDate, setStartDate] = useState(epicData.startAt);
+  const [endDate, setEndDate] = useState(epicData.endAt);
+
   return (
     <S.Form onSubmit={handleFormSubmit}>
-      <S.Input type="text" placeholder={epicData.name} value={value} onChange={handleChange} />
+      <S.Input
+        type="text"
+        placeholder={epicData.name}
+        value={value}
+        onChange={handleChange}
+        isTitle
+      />
+      <S.Label>
+        <span>시작일</span>
+        <S.Input
+          type="date"
+          value={formatDate(startDate)}
+          onChange={(e) => setStartDate(e.target.valueAsDate ?? new Date())}
+        />
+      </S.Label>
+      <S.Label>
+        <span>종료일</span>
+        <S.Input
+          type="date"
+          value={formatDate(endDate)}
+          onChange={(e) => setEndDate(e.target.valueAsDate ?? new Date())}
+        />
+      </S.Label>
     </S.Form>
   );
 };
--- client/src/components/EpicEditModal/style.ts
+++ client/src/components/EpicEditModal/style.ts
@@ -3,22 +3,38 @@ import styled from 'styled-components';
 const S = {
   Form: styled.form`
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
 
     min-width: 350px;
     padding: 16px;
   `,
-  Input: styled.input`
+  Input: styled.input<{ isTitle?: boolean }>`
     width: 100%;
     padding: 8px 16px;
-    margin-right: 16px;
+    margin-bottom: ${({ isTitle }) => (isTitle ? '16px' : '0')};
 
     background-color: ${({ theme }) => theme.color.gray100};
     color: ${({ theme }) => theme.color.gray400};
     border-radius: 8px;
 
-    font: ${({ theme }) => theme.font.body_regular};
+    font: ${({ theme, isTitle }) => (isTitle ? theme.font.body_medium : theme.font.body_regular)};
+  `,
+  Label: styled.label`
+    display: flex;
+    align-items: center;
+
+    font: ${({ theme }) => theme.font.bold_regular};
+    word-break: keep-all;
+
+    & + & {
+      margin-top: 8px;
+    }
+
+    & input {
+      margin-left: 16px;
+    }
   `,
 };
 
