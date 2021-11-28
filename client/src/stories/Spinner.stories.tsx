34bab969c003e214e9b29d6184a059a53c4ecf6c
Harry <haesoo9410@gmail.com>
2021-11-28 16:42:46 +0900

    chore: 스피너 스토리북 등록 [#192]

---
+++ client/src/stories/Spinner.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, Story } from '@storybook/react';
+import React from 'react';
+import { Spinner } from '@/lib/design';
+
+type SpinnerProps = {
+  widthLevel: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  heightValue: number;
+  colorValue: 'gray' | 'white' | 'blue' | 'red';
+};
+
+export default {
+  title: 'src/lib/design/Spinner',
+  component: Spinner,
+} as Meta;
+
+const Template: Story<SpinnerProps> = (args) => <Spinner {...args} />;
+
+export const DefaultSearchBar = (args: SpinnerProps) => <Template {...args}></Template>;
