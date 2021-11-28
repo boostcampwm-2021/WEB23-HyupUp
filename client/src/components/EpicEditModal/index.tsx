9c95b4c02a76eb4bb6a07fb3c9ac000b694f5db8
rudy3091 <grayblack313@gmail.com>
2021-11-29 00:47:01 +0900

    refactor: 에픽 수정 모달 수정
    confirm 버튼을 사용하도록 수정

--- client/src/components/EpicEditModal/index.tsx
+++ client/src/components/EpicEditModal/index.tsx
@@ -1,40 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import S from './style';
-import { updateEpicById } from '@/lib/api/epic';
 import { Button } from '@/lib/design';
-import { useEpicDispatch } from '@/lib/hooks/useContextHooks';
 import { EpicType } from '@/types/epic';
 
 interface EpicEditModalProps {
   epicData: EpicType;
-  setShowModal: (state: boolean) => void;
+  value: string;
+  handleChange: (e: React.ChangeEvent) => void;
+  handleFormSubmit: (e: React.FormEvent) => void;
 }
 
-const EpicEditModal = ({ epicData, setShowModal }: EpicEditModalProps) => {
-  const dispatchEpic = useEpicDispatch();
-  const [value, setValue] = useState(epicData.name);
-
-  const handleFormSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    const updatedEpic = { ...epicData, name: value };
-    updateEpicById(epicData.id, updatedEpic);
-    dispatchEpic({
-      type: 'UPDATE_EPIC',
-      epic: updatedEpic,
-    });
-    setShowModal(false);
-  };
-
-  const handleChange = (ev: React.ChangeEvent) => {
-    setValue((ev.target as HTMLInputElement).value);
-  };
-
+const EpicEditModal = ({ epicData, value, handleChange, handleFormSubmit }: EpicEditModalProps) => {
   return (
     <S.Form onSubmit={handleFormSubmit}>
       <S.Input type="text" placeholder={epicData.name} value={value} onChange={handleChange} />
-      <Button category="default" size="small" onClick={handleFormSubmit}>
-        OK
-      </Button>
     </S.Form>
   );
 };
