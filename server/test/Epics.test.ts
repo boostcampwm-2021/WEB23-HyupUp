86b743e589e6f8ba11debebbbd790daa7a6fc881
이찬호 <chanholee275@gmail.com>
2021-11-29 21:52:44 +0900

    feat: Epics test code 수정 및 jest path alias 설정
    cookie를 가지고 응답하여 401 error가 발생하지 않도록 코드 수정

--- server/test/Epics.test.ts
+++ server/test/Epics.test.ts
@@ -83,26 +83,47 @@ beforeAll(async () => {
 afterAll(async () => {
   const connection = await getConnection();
   await connection.query(`DELETE FROM ORGANIZATIONS;`);
+  await connection.query(`DELETE FROM EPICS;`);
   await connection.query(`DELETE FROM PROJECTS;`);
   await connection.query(`DELETE FROM USERS;`);
-  await connection.query(`DELETE FROM EPICS;`);
   await connection.close();
+});
+
+let Cookies = '';
+
+beforeEach((done) => {
+  request(app)
+    .post('/api/users/login')
+    .send({
+      email: 'test@test.com',
+      password: 'token',
+    })
+    .end(function (err, res) {
+      Cookies = res.headers['set-cookie'].pop().split(';')[0];
+      done();
+    });
 });
 
 // FIXME: 세션에 사용자 mock
 test('전체 에픽 목록을 조회한다.', async () => {
-  const res = await request(app).get('/api/epics?projectId=1');
+  const req = request(app).get('/api/epics?projectId=1');
+  req.set('Cookie', Cookies);
+  const res = await req;
   expect(res.status).toBe(200);
   expect(res.body).toEqual(mockData);
 });
 
 test('잘못된 projectId에 대해 응답코드 400을 반환한다.', async () => {
-  const res = await request(app).get('/api/epics?projectId=9999');
+  const req = request(app).get('/api/epics?projectId=9999');
+  req.set('Cookie', Cookies);
+  const res = await req;
   expect(res.status).toBe(400);
 });
 
 test('개별 에픽 정보 요청에 대한 응답을 반환한다.', async () => {
-  const res = await request(app).get('/api/epics/1');
+  const req = request(app).get('/api/epics/1');
+  req.set('Cookie', Cookies);
+  const res = await req;
   expect(res.status).toBe(200);
   expect(res.body).toEqual(mockData[0]);
   expect(res.body).toEqual(mockData[1]);
@@ -111,31 +132,37 @@ test('개별 에픽 정보 요청에 대한 응답을 반환한다.', async () =
 });
 
 test('잘못된 에픽 id로 보낸 요청에 대해 응답코드 404를 반환한다.', async () => {
-  const res = await request(app).get('/api/epics/9999');
+  const req = request(app).get('/api/epics/9999');
+  req.set('Cookie', Cookies);
+  const res = await req;
   expect(res.status).toBe(404);
 });
 
 test('에픽을 생성한다.', async () => {
-  const res = await request(app).post('/api/epics').send({
+  const req = request(app).post('/api/epics').send({
     name: '새롭게 생성한 에픽',
     projectId: 1,
     startAt: new Date(),
     endAt: new Date(),
     order: 4,
   });
+  req.set('Cookie', Cookies);
+  const res = await req;
   expect(res.status).toBe(201);
   expect(res.body).toEqual({ id: 5 });
 });
 
 test('잘못된 에픽 생성 요청에 대해 응답코드 400을 반환한다.', async () => {
-  const res = await request(app).post('/api/epics').send({
+  const req = request(app).post('/api/epics').send({
     name: '잘못된 에픽 생성 요청',
   });
+  req.set('Cookie', Cookies);
+  const res = await req;
   expect(res.status).toBe(400);
 });
 
 test('에픽을 수정한다.', async () => {
-  const res = await request(app)
+  const req = request(app)
     .patch('/api/epics/5')
     .send({
       name: '수정한 에픽',
@@ -144,14 +171,18 @@ test('에픽을 수정한다.', async () => {
       endAt: new Date('2021-11-03'),
       order: 4,
     });
+  req.set('Cookie', Cookies);
+  const res = await req;
   expect(res.status).toBe(200);
   expect(res.body).toEqual({});
 
   // FIXME: 이 상태로는 get 요청까지 validate 함
   // 어떻게 수정됐음을 확인할것인가?
-  const edited = await request(app).get('/api/epics/5');
-  expect(edited.status).toBe(200);
-  expect(res.body).toEqual({
+  const edited = request(app).get('/api/epics/5');
+  edited.set('Cookie', Cookies);
+  const res2 = await edited;
+  expect(res2.status).toBe(200);
+  expect(res2.body).toEqual({
     name: '수정한 에픽',
     projectId: 1,
     startAt: '2021-11-03 00:00:00',
@@ -161,18 +192,24 @@ test('에픽을 수정한다.', async () => {
 });
 
 test('잘못된 에픽 수정 요청에 대해 응답코드 400을 반환한다.', async () => {
-  const res = await request(app).patch('/api/epics/9999').send({
+  const req = request(app).patch('/api/epics/9999').send({
     name: '잘못 요청한 에픽',
   });
+  req.set('Cookie', Cookies);
+  const res = await req;
   expect(res.status).toBe(400);
 });
 
 test('에픽을 삭제한다.', async () => {
-  const res = await request(app).delete('/api/epics/5');
+  const req = request(app).delete('/api/epics/5');
+  req.set('Cookie', Cookies);
+  const res = await req;
   expect(res.status).toBe(200);
 });
 
 test('잘못된 id로 에픽 삭제 요청을 보낼 시 응답코드 404을 반환한다.', async () => {
-  const res = await request(app).delete('/api/epics/9999');
+  const req = request(app).delete('/api/epics/9999');
+  req.set('Cookie', Cookies);
+  const res = await req;
   expect(res.status).toBe(404);
 });
