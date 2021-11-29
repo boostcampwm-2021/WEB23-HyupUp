f4a545abd228a7bb71a61848154c00ee21a25f9f
이찬호 <chanholee275@gmail.com>
2021-11-29 18:31:44 +0900

    fix: 상대 경로를 path alias를 활용해 경로 변경 [#210]

--- server/src/Users/Users.router.ts
+++ server/src/Users/Users.router.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authValidator } from '../utils/authValidator';
+import { authValidator } from '@/utils/authValidator';
 import {
   deleteUserById,
   getUsersInfoWithProject,
