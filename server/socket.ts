d89fbc66f0246f845445331270400a70605f18b8
ingong <insong1216@gmail.com>
2021-11-29 18:41:39 +0900

    feat: Server 스토리 삭제에 대한 소켓 이벤트 전송 로직 구현 [#211]

--- server/socket.ts
+++ server/socket.ts
@@ -69,6 +69,10 @@ io.on('connection', (socket) => {
   socket.on('NEW_STORY', (storyId: number) => {
     socket.to(socket.data.roomName).emit('NEW_STORY', storyId);
   });
+
+  socket.on('DELETE_STORY', (storyId: number) => {
+    socket.to(socket.data.roomName).emit('DELETE_STORY', storyId);
+  });
 });
 
 export default io;
