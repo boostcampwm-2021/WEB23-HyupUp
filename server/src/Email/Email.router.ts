d86f3a464ac9e375218c966178c2470e11215c42
이찬호 <chanholee275@gmail.com>
2021-11-29 18:31:44 +0900

    fix: 상대 경로를 path alias를 활용해 경로 변경 [#210]

--- server/src/Email/Email.router.ts
+++ server/src/Email/Email.router.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authValidator } from '../utils/authValidator';
+import { authValidator } from '@/utils/authValidator';
 import { inviteByEmail, isValidEmail } from './Email.controller';
 
 const router = express.Router();
--- server/src/Todo/Todo.router.ts
+++ server/src/Todo/Todo.router.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authValidator } from '../utils/authValidator';
+import { authValidator } from '@/utils/authValidator';
 import { createTodo, deleteTodo, updateTodo } from './Todo.controller';
 
 const router = express.Router();
