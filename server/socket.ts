0dbd56c7acd79b733b2959fde26aee57667ab155
이찬호 <chanholee275@gmail.com>
2021-11-29 12:02:44 +0900

    fix: redis client 생성 시, 사용하는 변수 환경 변수로 교체

--- server/socket.ts
+++ server/socket.ts
@@ -4,7 +4,10 @@ import { createAdapter } from '@socket.io/redis-adapter';
 import { RedisClient } from 'redis';
 
 const io = new Server();
-const pubClient = new RedisClient({ host: 'localhost', port: 6379 });
+const pubClient = new RedisClient({
+  host: process.env.REDIS_HOST,
+  port: +(process.env.REDIS_PORT as string),
+});
 const subClient = pubClient.duplicate();
 
 io.adapter(createAdapter(pubClient, subClient));
