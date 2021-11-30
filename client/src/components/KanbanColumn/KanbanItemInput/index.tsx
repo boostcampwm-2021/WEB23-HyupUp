24cbec1ffffb52de52b3ddfed9c7dde4ca8b05b8
ingong <insong1216@gmail.com>
2021-11-30 17:29:48 +0900

    refactor: 스토리의 이름 수정을 Selector 로 수정 [#233]
    - 이름을 변경하는 경우 스토리 하나에 대한 수정만 있으면 되기 때문

--- client/src/components/KanbanColumn/KanbanItemInput/index.tsx
+++ client/src/components/KanbanColumn/KanbanItemInput/index.tsx
@@ -1,12 +1,12 @@
 import React, { useRef } from 'react';
-import { InputContainer, Input } from './style';
 import { useSetRecoilState } from 'recoil';
+import { InputContainer, Input } from './style';
 import { useInput } from '@/lib/hooks';
 import { updateStoryWithName } from '@/lib/api/story';
 import { StoryType } from '@/types/story';
 import { EpicType } from '@/types/epic';
-import storyListAtom from '@/recoil/story';
 import { useSocketSend } from '@/lib/hooks';
+import { storyState } from '@/recoil/story';
 
 const KanbanInput = ({
   story,
@@ -17,17 +17,14 @@ const KanbanInput = ({
   epic: EpicType | undefined;
   isHover: boolean;
 }) => {
-  const setStoryListState = useSetRecoilState(storyListAtom);
   const { key, value, onChange } = useInput('');
+  const updateStoryName = useSetRecoilState(storyState(key));
   const emitUpdateStory = useSocketSend('UPDATE_STORY');
   const inputRef = useRef<HTMLInputElement>(null);
 
   const useUpdateStoryName = async () => {
     if (key < 0) return;
-    setStoryListState((prev) => [
-      ...prev.filter((v) => v.id !== key),
-      { status: 'TODO', id: key, order: story.order, name: value },
-    ]);
+    updateStoryName({ status: 'TODO', id: key, order: story.order, name: value });
     await updateStoryWithName({ status: 'TODO', id: key, order: story.order, name: value });
     emitUpdateStory(key);
   };
