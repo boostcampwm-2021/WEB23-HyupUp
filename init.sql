0420cb1322251d17e45265b858f15e0b6ad6b48f
이찬호 <chanholee275@gmail.com>
2021-11-30 18:48:30 +0900

    feat: init.sql에 dummy data 추가

--- init.sql
+++ init.sql
@@ -49,6 +49,32 @@ INSERT INTO `HYUPUP`.`EPICS` (`PROJECT_ID`, `NAME`, `START_AT`, `END_AT`, `ORDER
 INSERT INTO `HYUPUP`.`EPICS` (`NAME`, `PROJECT_ID`,  `START_AT`, `END_AT`, `ORDER`) VALUES ( '마이그레이션', '2', NOW(), NOW(), 1); 
 INSERT INTO `HYUPUP`.`EPICS` (`NAME`, `PROJECT_ID`,  `START_AT`, `END_AT`, `ORDER`) VALUES ( '칸반보드 구현', '2', NOW(), NOW(), 1); 
 
+INSERT INTO `HYUPUP`.`EPICS` (`NAME`, `PROJECT_ID`,  `START_AT`, `END_AT`, `ORDER`, 'ID') VALUES ( '로그인 페이지 구현', '1', '2021-11-23', '2021-11-25', 4, 4); 
+INSERT INTO `HYUPUP`.`EPICS` (`NAME`, `PROJECT_ID`,  `START_AT`, `END_AT`, `ORDER`, 'ID') VALUES ( '작업페이지 기본 구현', '1','2021-11-09', '2021-11-10' 5, 5); 
+INSERT INTO `HYUPUP`.`EPICS` (`NAME`, `PROJECT_ID`,  `START_AT`, `END_AT`, `ORDER`, 'ID') VALUES ( '로드맵 구현', '1', '2021-11-11','2021-11-25', 6, 6);
+
+
+INSERT INTO STORIES(NAME, STATUS, EPIC_ID, PROJECT_ID, `ORDER`) VALUES('로그인 페이지에서 이메일과 비밀번호를 입력하면 메인 페이지로 이동한다.', '3', 4, 1, 4);
+INSERT INTO STORIES(NAME, STATUS, EPIC_ID, PROJECT_ID, `ORDER`) VALUES('작업페이지 아이콘 클릭 시 작업 페이지로 이동한다.', '3', 5, 1, 5);
+INSERT INTO STORIES(NAME, STATUS, EPIC_ID, PROJECT_ID, `ORDER`) VALUES('사이드바의 로드맵 클릭 시 로드맵 페이지가 표시된다.', '3', 6, 1, 6);
+
+INSERT INTO `HYUPUP`.`TASKS` (`STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '1', '1', '5', '1', '첫 번째 프로젝트에 대한 작업페이지를 디폴트로 표시한다.');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '1', '1', '5', '1', '프로젝트 드롭다운에 프로젝트 목록이 표시된다.');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '1', '1', '5', '1', '로드맵, 칸반보드, 백로그 클릭시 해당 페이지로 이동한다.');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '1', '1', '5', '1', '드롭다운에서 프로젝트 클릭시 해당 프로젝트의 작업페이지가 표시된다.');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '0', '1', '5', '1', '가장 마지막에 본 작업페이지를 기본으로 표시한다.');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '1', '1', '6', '1', '사이드바의 로드맵 글자가 활성화 된다.');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '1', '1', '6', '1', '전체적인 UI, 현재 프로젝트의 에픽 목록과 추가버튼이 표시된다.');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '1', '1', '6', '1', '추가버튼을 눌러 에픽을 추가할 수 있다. + 소켓 이벤트 emit');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ('1', '1', '6', '1', '오른쪽 영역에 한 달을 열로 가지고 에픽 갯수 만큼의 행을 가진 표가 생성된다');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '1', '1', '6', '1', '좌우 버튼을 클릭해서 일주일단위로 변경한다.');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '1', '1', '6', '1', '에픽의 시작일과 종료일을 보여주는 막대그래프를 표시한다.');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ('1', '1', '6', '1', '막대그래프의 크기를 드래그앤 드랍으로 조정하여 시작일과 종료일을 변경한다. (소켓)');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '1', '1', '6', '1', '칸반보드의 상태에 따라 색상이 변경된다. ');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '1', '1', '4', '1', '유저 정보가 없다면 렌딩페이지 / 있다면 홈페이지로 이동한다.');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '1', '1', '4', '1', '렌딩페이지에서 이메일로 로그인이 가능하다.');
+INSERT INTO `HYUPUP`.`TASKS` ( `STATUS`, `PROJECT_ID`, `STORY_ID`, `USER_ID`, `NAME`) VALUES ( '1', '1', '4', '1', '로그인 후 정보가 있다면 세션 저장 후 홈페이지로 이동한다.');
+
 INSERT INTO STORIES(NAME, STATUS, EPIC_ID, PROJECT_ID, `ORDER`) VALUES('첫 스토리', '1', 1, 1, 0);
 INSERT INTO STORIES(NAME, STATUS, EPIC_ID, PROJECT_ID, `ORDER`) VALUES('두 번째 스토리', '2', 1, 1, 1);
 
