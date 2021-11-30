e2589aa967a964b121395a5802c857136016a5a2
이찬호 <chanholee275@gmail.com>
2021-11-30 21:19:42 +0900

    feat: 이메일 링크를 클릭하면 발생하는 로직 테스트 코드 작성

--- server/test/Email.test.ts
+++ server/test/Email.test.ts
@@ -1,5 +1,9 @@
 import { ConnectionOptions, createConnection, getConnection } from 'typeorm';
+import { createClient, RedisClient } from 'redis';
+import { promisify } from 'util';
 import { entities } from '../src';
+import jwt from 'jsonwebtoken';
+import { v4 } from 'uuid';
 import app from '../app';
 import request from 'supertest';
 import supertest from 'supertest';
@@ -73,5 +77,44 @@ describe('sending Email', () => {
       organizationId: 1,
     });
     expect(res.status).toBe(201);
+  });
+});
+
+describe('verifying Email', () => {
+  const validKey = v4();
+  const invalidKey = v4();
+  const redisConnection: RedisClient = createClient({
+    host: process.env.REDIS_HOST,
+    port: +(process.env.REDIS_PORT as string),
+  });
+  const validToken = jwt.sign({ id: validKey }, process.env.SECRET as string, {
+    algorithm: 'HS256',
+    expiresIn: '1h',
+  });
+
+  const invalidToken = jwt.sign({ id: invalidKey }, process.env.SECRET as string, {
+    algorithm: 'HS256',
+    expiresIn: '1s',
+  });
+  const setAsync = promisify(redisConnection.set).bind(redisConnection);
+
+  test('올바른 token을 통한 링크인 경우', async () => {
+    await setAsync(validKey, JSON.stringify({ organizationId: 1, email: 'test10@test.com' }));
+    const res = await supertest(app).get(`/api/email/verify/${validToken}`);
+    redisConnection.del(validKey);
+    expect(res.status).toBe(302);
+  });
+  test('올바른 token이 만료된 경우', async () => {
+    await setAsync(invalidKey, JSON.stringify({ organizationId: 1, email: 'test10@test.com' }));
+    const res = await supertest(app).get(`/api/email/verify/${invalidToken}`);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    redisConnection.del(invalidKey);
+    expect(res.status).toBe(401);
+  });
+  test('token이 없는 경우', async () => {
+    await setAsync('1234', JSON.stringify({ organizationId: 1, email: 'test10@test.com' }));
+    const res = await supertest(app).get(`/api/email/verify/1234`);
+    redisConnection.del('1234');
+    expect(res.status).toBe(401);
   });
 });
