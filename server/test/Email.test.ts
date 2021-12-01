950f4142b76a58cf507880f327b9aec7c08f89c4
이찬호 <chanholee275@gmail.com>
2021-12-01 23:38:29 +0900

    refactor: test code의 mock 데이터를 다른 테스트와 일치

--- server/test/Email.test.ts
+++ server/test/Email.test.ts
@@ -1,47 +1,14 @@
-import { ConnectionOptions, createConnection, getConnection } from 'typeorm';
 import { createClient, RedisClient } from 'redis';
 import { promisify } from 'util';
-import { entities } from '../src';
 import jwt from 'jsonwebtoken';
 import { v4 } from 'uuid';
 import app from '../app';
 import request from 'supertest';
 import supertest from 'supertest';
+import { afterAllFunction, beforeAllfunction } from '.';
 
-beforeAll(async () => {
-  const dbConfig = {
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_TEST,
-  };
-
-  const connectionOptions: ConnectionOptions = {
-    type: 'mysql',
-    synchronize: true,
-    entities: entities,
-    ...dbConfig,
-  };
-
-  const connection = await createConnection(connectionOptions);
-  await connection.synchronize();
-  await connection.query(`INSERT INTO ORGANIZATIONS(ROOM) VALUES('TEAM42');`);
-  await connection.query(`INSERT INTO PROJECTS(NAME) VALUES('HYUPUP');`);
-  await connection.query(
-    `INSERT INTO USERS(NAME, JOB, EMAIL, IMAGE_URL, ADMIN, PASSWORD, ORGANIZATION_ID)` +
-      `VALUES('hm', 'developer', 'test@test.com', 'url', 1,` +
-      `'$2b$10$e3kxJ2jBwf/o5rpNgG.rqe/fvHyPxl2UH6r16ySb18IsLEeeGMwye', 1);`,
-  );
-});
-
-afterAll(async () => {
-  const connection = await getConnection();
-  await connection.query(`DELETE FROM USERS;`);
-  await connection.query(`DELETE FROM ORGANIZATIONS;`);
-  await connection.query('ALTER TABLE ORGANIZATIONS AUTO_INCREMENT = 1');
-  await connection.query('ALTER TABLE USERS AUTO_INCREMENT = 1');
-  await connection.close();
-});
+beforeAll(beforeAllfunction);
+afterAll(afterAllFunction);
 
 let Cookies = '';
 beforeAll((done) => {
