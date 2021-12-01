daf85d0e1b3e53fc6ae1aab0a0b7e31e992e7a03
rudy3091 <grayblack313@gmail.com>
2021-12-01 18:55:53 +0900

    fix: 에픽 생성시 글자제한 설정 [#208]
    255자의 길이제한을 넘으면 토스트 알림을 띄우도록 수정

--- client/src/components/EpicPlaceholder/index.tsx
+++ client/src/components/EpicPlaceholder/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import S from './style';
 import { Button, Modal } from '@/lib/design';
+import { checkStringInput } from '@/lib/utils/bytes';
+import { toast } from 'react-toastify';
+import { errorMessage } from '@/lib/common/message';
 
 interface EpicPlaceholderProps {
   visible: boolean;
@@ -13,6 +16,10 @@ const EpicPlaceholder = ({ visible, setVisible, handleSubmit }: EpicPlaceholderP
 
   const handleFormSubmit = (ev: React.FormEvent) => {
     ev.preventDefault();
+    if (!checkStringInput(value)) {
+      toast.error(errorMessage.EPIC_TITLE_LENGTH_LIMIT);
+      return;
+    }
     handleSubmit(value);
     setValue('');
   };
@@ -32,7 +39,7 @@ const EpicPlaceholder = ({ visible, setVisible, handleSubmit }: EpicPlaceholderP
       <S.Container onSubmit={handleFormSubmit}>
         <S.Input
           type="text"
-          placeholder="에픽 제목을 입력하세요"
+          placeholder="에픽 제목을 입력하세요 (255자 이내)"
           value={value}
           onChange={handleChange}
         />
