006ee9dd35c17b2e755d58ba01dba7ba3b97d921
rudy3091 <grayblack313@gmail.com>
2021-12-02 23:29:46 +0900

    fix: p 태그 제거
    p 태그 아래에 div 태그를 넣을 수 없다는 경고로 인한 수정

--- client/src/components/EpicColorInfo/index.tsx
+++ client/src/components/EpicColorInfo/index.tsx
@@ -15,24 +15,22 @@ const EpicColorInfo = () => {
     <S.Container onMouseOver={handleOver} onMouseOut={handleOut}>
       <img src={infoIcon} alt="info icon" />
       <S.TooltipContainer hidden={showInfo}>
-        <p>
-          <S.Line>
-            <S.Emphasize status={'UNINITIALIZED'}>회색</S.Emphasize> 막대는 연동된 스토리가 없는
-            에픽입니다.
-          </S.Line>
-          <S.Line>
-            <S.Emphasize status={'NOT_STARTED'}>빨간색</S.Emphasize> 막대는 연동된 스토리들이 모두
-            진행중이 아닌 에픽입니다.
-          </S.Line>
-          <S.Line>
-            <S.Emphasize status={'STARTED'}>초록색</S.Emphasize> 막대는 연동된 스토리들이 하나라도
-            진행중인 에픽입니다.
-          </S.Line>
-          <S.Line>
-            <S.Emphasize status={'ALL_DONE'}>파란색</S.Emphasize> 막대는 연동된 스토리들이 모두
-            완료된 에픽입니다.
-          </S.Line>
-        </p>
+        <S.Line>
+          <S.Emphasize status={'UNINITIALIZED'}>회색</S.Emphasize> 막대는 연동된 스토리가 없는
+          에픽입니다.
+        </S.Line>
+        <S.Line>
+          <S.Emphasize status={'NOT_STARTED'}>빨간색</S.Emphasize> 막대는 연동된 스토리들이 모두
+          진행중이 아닌 에픽입니다.
+        </S.Line>
+        <S.Line>
+          <S.Emphasize status={'STARTED'}>초록색</S.Emphasize> 막대는 연동된 스토리들이 하나라도
+          진행중인 에픽입니다.
+        </S.Line>
+        <S.Line>
+          <S.Emphasize status={'ALL_DONE'}>파란색</S.Emphasize> 막대는 연동된 스토리들이 모두 완료된
+          에픽입니다.
+        </S.Line>
       </S.TooltipContainer>
     </S.Container>
   );
