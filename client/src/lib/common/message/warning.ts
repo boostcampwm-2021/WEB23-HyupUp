0f1f80bfbb6c4f72dae97c4dee506d8f99548555
ingong <insong1216@gmail.com>
2021-12-04 10:02:03 +0900

    fix: 프로젝트 아이디가 없을 때 스토리생성 불가능하게 하고 토스트 에러 추가[#278] 가

--- client/src/lib/common/message/warning.ts
+++ client/src/lib/common/message/warning.ts
@@ -1,9 +1,11 @@
 export const ADMIN_ACCESS = '관리자 권한이 필요합니다.';
 export const MOVE_PROJECT = '현재 속한 프로젝트로만 이동이 가능합니다.';
 export const DELETE_MYSELF = '자신의 상태는 변경하실 수 없습니다.';
+export const CREATE_STORY_WITHOUT_AUTH = '프로젝티 관리 탭에서 프로젝트를 생성해주세요.';
 
 export default {
   ADMIN_ACCESS,
   MOVE_PROJECT,
   DELETE_MYSELF,
+  CREATE_STORY_WITHOUT_AUTH,
 };
