057dba7bd16c5fc5703416c7535877fa76da8af2
Harry <haesoo9410@gmail.com>
2021-12-14 21:14:48 +0900

    feat: tasksSelector 추가 [#285] 스토리 아이디에 따른 테스크 배열을 보여주는 셀렉터 추가

--- client/src/recoil/story/selector.ts
+++ client/src/recoil/story/selector.ts
@@ -2,29 +2,43 @@ import { DefaultValue, selectorFamily } from 'recoil';
 import produce from 'immer';
 import storyListAtom from './atom';
 import { StoryType } from '@/types/story';
+import { getTasksByStoryId } from '@/lib/api/task';
 
 export const storyState = selectorFamily<StoryType | undefined, number>({
   key: 'storyWithIdSelector',
-  get: (id) => ({ get }) => {
-    const storyList = get(storyListAtom);
-    return storyList?.find((story) => story.id === id);
-  },
-  set: (id: number) => ({ set, get }, newValue) => {
-    set(
-      storyListAtom,
-      newValue instanceof DefaultValue
-        ? get(storyListAtom)
-        : (prev) =>
-            produce(prev, (draft) => {
-              const newStory = draft?.find((story) => story.id === id);
-              if (!newStory || !newValue) return;
-              newStory.name = newValue.name;
-              newStory.id = newValue.id;
-              newStory.order = newValue.order;
-              newStory.status = newValue.status;
-              newStory.epicId = newValue.epicId;
-              newStory.projectId = newValue.projectId;
-            }),
-    );
+  get:
+    (id) =>
+    ({ get }) => {
+      const storyList = get(storyListAtom);
+      return storyList?.find((story) => story.id === id);
+    },
+  set:
+    (id: number) =>
+    ({ set, get }, newValue) => {
+      set(
+        storyListAtom,
+        newValue instanceof DefaultValue
+          ? get(storyListAtom)
+          : (prev) =>
+              produce(prev, (draft) => {
+                const newStory = draft?.find((story) => story.id === id);
+                if (!newStory || !newValue) return;
+                newStory.name = newValue.name;
+                newStory.id = newValue.id;
+                newStory.order = newValue.order;
+                newStory.status = newValue.status;
+                newStory.epicId = newValue.epicId;
+                newStory.projectId = newValue.projectId;
+              }),
+      );
+    },
+});
+
+export const tasksSelector = selectorFamily({
+  key: 'tasksSelector',
+  get: (storyId: number) => async () => {
+    const taskList = await getTasksByStoryId(storyId);
+    if (taskList?.length) return taskList;
+    return [];
   },
 });
