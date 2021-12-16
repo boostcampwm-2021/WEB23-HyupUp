0fed45688fc0ea9a47baa2186a6a48a912372f43
Harry <haesoo9410@gmail.com>
2021-12-16 20:24:30 +0900

    refactor: story selector 코드 개선 동일 로직으로 코드 길이 줄임

--- client/src/recoil/story/selector.ts
+++ client/src/recoil/story/selector.ts
@@ -21,14 +21,9 @@ export const storyState = selectorFamily<StoryType | undefined, number>({
           ? get(storyListAtom)
           : (prev) =>
               produce(prev, (draft) => {
-                const newStory = draft?.find((story) => story.id === id);
-                if (!newStory || !newValue) return;
-                newStory.name = newValue.name;
-                newStory.id = newValue.id;
-                newStory.order = newValue.order;
-                newStory.status = newValue.status;
-                newStory.epicId = newValue.epicId;
-                newStory.projectId = newValue.projectId;
+                const newStoryIdx = draft?.findIndex((story) => story.id === id);
+                if (newStoryIdx === -1 || !newValue) return;
+                draft[newStoryIdx] = newValue;
               }),
       );
     },
@@ -38,7 +33,6 @@ export const tasksSelector = selectorFamily({
   key: 'tasksSelector',
   get: (storyId: number) => async () => {
     const taskList = await getTasksByStoryId(storyId);
-    if (taskList?.length) return taskList;
-    return [];
+    return taskList?.length ? taskList : [];
   },
 });
