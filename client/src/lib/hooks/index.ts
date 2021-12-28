73aaa5adf0dda83b083fdff3d08a245a70c7dccb
rudy3091 <grayblack313@gmail.com>
2021-12-29 00:48:37 +0900

    fix: useSocketSendUser 훅 제거
    중복 훅 구현 제거
    하나의 파라미터를 전달할 땐 useSocketSendUser 훅과 같은 동작을 보장

--- client/src/lib/hooks/index.ts
+++ client/src/lib/hooks/index.ts
@@ -2,5 +2,4 @@ export { default as useInput } from './useInput';
 export { default as useOutSideClick } from './useOutSideClick';
 export { default as useSocketSend } from './useSocketSend';
 export { default as useSocketReceive } from './useSocketReceive';
-export { default as useSocketSendUser } from './useSocketSendUser';
 export { default as useTabs } from './useTabs';
